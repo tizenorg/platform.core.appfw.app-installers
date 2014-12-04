1f9b42479111d2d20fe36b0fa1802473b0e562bb
Tomasz Iwanek <t.iwanek@samsung.com>
2014-12-04 11:16:01 +0100

    Signature implementation moved from xwalk
    This is digital signature implementation moved from xwalk.
    It is based on following PRs:
     - https://github.com/crosswalk-project/crosswalk/pull/2169
     - https://github.com/crosswalk-project/crosswalk/pull/2291
     - https://github.com/crosswalk-project/crosswalk/pull/2422
    
    There are some not solved issues:
     - test correctness for valid widgets,
     - seperation from managing root certificate,
     - style of this code.
    
    [[ PLEASE NOTE THE COMMIT CONTAINS CROSSWALK'S BSD-STYLE LICENSE ]]
    
    Change-Id: I42db7e8a02b44a88ca88d143ad034fd1e253f3e2

---
+++ signature/include/logging.h
@@ -0,0 +1,30 @@
+// Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef UTILS_LOGGING_H_
+#define UTILS_LOGGING_H_
+
+#include <iostream>
+#include <sstream>
+
+// TODO(tiwanek): Logging...
+
+namespace detail {
+
+class LogCatcher {
+ public:
+  LogCatcher() { }
+  void operator&(const std::ostream& str) const {
+    // TODO(tiwanek): this cast is error-prone - fix it
+    std::cerr << static_cast<const std::ostringstream*>(&str)->str()
+              << std::endl;
+  }
+};
+
+}
+
+#define LOG(LEVEL)                                                             \
+    ::detail::LogCatcher() & std::ostringstream() << "[" << #LEVEL << "] "               \
+
+#endif  // UTILS_LOGGING_H_
