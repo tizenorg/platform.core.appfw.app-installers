fe843788303195ada826eaa987c77e61b510b8f3
Tomasz Iwanek <t.iwanek@samsung.com>
2014-12-04 11:16:01 +0100

    Signature implementation moved from xwalk
    This is digital signature implementation moved from xwalk.
    It is based on following PRs:
     - https://github.com/crosswalk-project/crosswalk/pull/2169
     - https://github.com/crosswalk-project/crosswalk/pull/2291
     - https://github.com/crosswalk-project/crosswalk/pull/2422
    
    There are some not solved issues:
     - test correctness for valid widgets,
     - seperation from managing root certificate,
     - style of this code.
    
    [[ PLEASE NOTE THE COMMIT CONTAINS CROSSWALK'S BSD-STYLE LICENSE ]]
    
    Change-Id: I42db7e8a02b44a88ca88d143ad034fd1e253f3e2
    Signed-off-by: Pawel Sikorski <p.sikorski@samsung.com>

---
+++ signature/include/marcos.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef UTILS_MARCOS_H_
+#define UTILS_MARCOS_H_
+
+#define DISALLOW_COPY_AND_ASSIGN(CLASS)            \
+  CLASS(const CLASS&) = delete;                    \
+  CLASS& operator=(const CLASS&) = delete          \
+
+#endif  // UTILS_MARCOS_H_
---
+++ signature/include/signature_xmlsec_adaptor.h
@@ -0,0 +1,27 @@
+// Copyright (c) 2014 Intel Corporation. All rights reserved.
+// Copyright (c) 2014 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache 2.0 license that can be
+// found in the LICENSE file.
+// Part of this file is redistributed from crosswalk project under BDS-style
+// license. Check LICENSE-xwalk file.
+
+#ifndef SIGNATURE_SIGNATURE_XMLSEC_ADAPTOR_H_
+#define SIGNATURE_SIGNATURE_XMLSEC_ADAPTOR_H_
+
+#include <boost/filesystem/path.hpp>
+
+#include <signature_data.h>
+
+namespace signature {
+
+class SignatureXmlSecAdaptor {
+ public:
+  static bool ValidateFile(const SignatureData& signature_data);
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(SignatureXmlSecAdaptor);
+};
+
+}  // namespace signature
+
+#endif  // COMMON_STEP_SIGNATURE_SIGNATURE_XMLSEC_ADAPTOR_H_
