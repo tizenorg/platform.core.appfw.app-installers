6581014ec834d9715f8a02a2963046c81a1957cc
Tomasz Weglarski <t.weglarski@samsung.com>
2015-01-08 11:35:06 +0100

    Widget parser test
    This is simple HOWTO use widget parser library.
    
    Change-Id: I9cda4679d5c8f5572ca114d0d8ead2ee4391bf4c

--- src/wgt/wgt_backend.cc
+++ src/wgt/wgt_backend.cc
@@ -17,7 +17,53 @@
 #include "common/step/step_signature.h"
 #include "common/step/step_copy.h"
 
+#include <iostream>
+#include <widget-manifest-parser/widget-manifest-parser.h>
+
+bool ParserTest(int argc, char **argv) {
+  if (argc != 3)
+    return true;
+
+  std::string test_switch = argv[1];
+  std::string file = argv[2];
+
+  if (test_switch != "-pt")
+    return true;
+
+  std::cout << "Parsing file " << file << std::endl;
+
+  const ManifestData* data = nullptr;
+  const char* error = nullptr;
+  if (!ParseManifest(file.c_str(), &data, &error)) {
+    std::cout << "Parse failed. " <<  error << std::endl;
+    if (!ReleaseData(data, error))
+      std::cout << "Release data failed." << std::endl;
+    return false;
+  }
+
+  std::cout << "Read data -[ " << std::endl;
+  std::cout << "  application_name = " <<  data->application_name << std::endl;
+  std::cout << "  required_version = " <<  data->required_version << std::endl;
+  std::cout << "  privilege_count  = " <<  data->privilege_count << std::endl;
+
+  std::cout << "  privileges -[" << std::endl;
+  for (unsigned int i = 0; i < data->privilege_count; ++i)
+    std::cout << "    " << data->privilege_list[i] << std::endl;
+  std::cout << "  ]-" << std::endl;
+  std::cout << "]-" << std::endl;
+
+  if (!ReleaseData(data, error)) {
+    std::cout << "Release data failed." << std::endl;
+    return false;
+  }
+
+  return false;
+}
+
 int main(int argc, char **argv) {
+  if (!ParserTest(argc, argv))
+    return 0;
+
   /* get request data */
   pkgmgr_installer *pi = pkgmgr_installer_new();
   if (!pi)
