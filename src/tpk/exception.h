dc209d4fd462d555206ea3c2bdc9bbb5efdc67d2
Youmin Ha <youmin.ha@samsung.com>
2015-01-30 13:38:00 +0900

    Add TPK backend
    Native tpk backend implementation: First version.
    Only simple tizen-manifest.xml format can be parsed for now.
    
    Signed-Off-By: Youmin Ha <youmin.ha@samsung.com>
    Change-Id: I95037fa6b08639c6dd008d0ffb796cfd375c8060

---
+++ src/tpk/exception.h
@@ -0,0 +1,43 @@
+/* Copyright 2015 Samsung Electronics, license APACHE-2.0, see LICENSE file */
+#ifndef TPK_EXCEPTION_H_
+#define TPK_EXCEPTION_H_
+
+#include <execinfo.h>
+#include <unistd.h>
+#include <exception>
+#include <iostream>
+
+namespace tpk {
+
+class Exception: public std::exception {
+ public:
+  Exception() {
+    this->message = "";
+    storeStackTrace();
+  }
+  explicit Exception(const char *message) {
+    this->message = message;
+    storeStackTrace();
+  }
+  virtual void storeStackTrace() {
+    size = backtrace(stack, MAX_STACKSIZE);
+  }
+  virtual void printStackTrace() {
+    std::cerr << "Exception: " << message << std::endl;
+    // Exclude top 1 stack entry (storeStackTrace)
+    backtrace_symbols_fd(stack+1, size-1, STDERR_FILENO);
+  }
+
+
+ protected:
+  const char* message;
+
+
+ private:
+  static const size_t MAX_STACKSIZE = 100;
+  size_t size;
+  void *stack[MAX_STACKSIZE];
+};
+
+}  // namespace tpk
+#endif  // TPK_EXCEPTION_H_
---
+++ src/tpk/test/xmlparse.cc
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <iostream>
+#include <libxml/xmlreader.h>
+
+using namespace std;
+
+
+static void
+ProcessNode(xmlTextReaderPtr reader) {
+	const xmlChar *name, *value;
+	xmlReaderTypes type;
+
+	name = xmlTextReaderConstName(reader);
+	if (name == NULL) {
+		name = BAD_CAST "--";
+	}
+
+	value = xmlTextReaderConstValue(reader);
+	type = (xmlReaderTypes) xmlTextReaderNodeType(reader);
+
+	printf("depth(%d) type(%d) %s isEmpty(%d) hasval(%d)",
+			xmlTextReaderDepth(reader),
+			type,
+			name,
+			xmlTextReaderIsEmptyElement(reader),
+			xmlTextReaderHasValue(reader));
+	if (value == NULL) {
+		printf("\n");
+	} else {
+		if (xmlStrlen(value) > 40) {
+			printf(" %.40s...\n", value);
+		} else {
+			printf(" %s\n", value);
+		}
+	}
+}
+
+
+static void
+StreamFile(const char* filepath) {
+	xmlTextReaderPtr reader;
+	int ret;
+
+	reader = xmlReaderForFile(filepath, NULL, 0);
+	if (reader != NULL) {
+		ret = xmlTextReaderRead(reader);
+		while (ret == 1) {
+			ProcessNode(reader);
+			ret = xmlTextReaderRead(reader);
+		}
+		xmlFreeTextReader(reader);
+		if(ret != 0) {
+			cerr << filepath << ": parse failure" << endl;
+		}
+	} else {
+		cerr << "File open failure: " << filepath << endl;
+	}
+}
+
+int
+main(int argc, char* argv[]) {
+	if(argc != 2) return 1;
+
+	LIBXML_TEST_VERSION
+
+	StreamFile(argv[1]);
+
+	xmlCleanupParser();
+	xmlMemoryDump();
+
+	return 0;
+}
+
