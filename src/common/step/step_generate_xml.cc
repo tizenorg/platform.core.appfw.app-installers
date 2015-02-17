24f138e3e28dc805f10fbf30ee438b6472b4f22a
Youmin Ha <youmin.ha@samsung.com>
2015-02-17 11:52:55 +0900

    Fix wrong manifest xml file name
    The manifest file name is wrong. Desired xml filename is
    'org.tizen.hello.xml', but the current implementation removes last
    extension and put '.xml' at the end.
    This commit fixes the name of the generated manifest xml file.
    For wgt packages, '*.wgt.xml' is present, but that's not the issue.
    
    Change-Id: I57e2506898f17c0853fa42f3172225b69cd9819c
    Signed-Off-By: Youmin Ha <youmin.ha@samsung.com>

--- src/common/step/step_generate_xml.cc
+++ src/common/step/step_generate_xml.cc
@@ -26,7 +26,7 @@ Step::Status StepGenerateXml::process() {
 
   fs::path xml_path = fs::path(getUserManifestPath(context_->uid()))
       / fs::path(context_->pkgid());
-  xml_path.replace_extension(".xml");
+  xml_path += ".xml";
 
   context_->set_xml_path(xml_path.string());
   boost::system::error_code error;
