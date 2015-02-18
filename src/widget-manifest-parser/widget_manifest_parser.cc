7bfaae8ad70a018540ce7233c4ac5bd2a18a0409
Jakub Izydorczyk <j.izydorczyk@samsung.com>
2015-02-18 09:57:12 +0100

    [Widget manifest parser] Remove C API from parser
    Change C API functions into class containing ParseManifest,
    GetShortName and GetRequiredAPIVersion methods, which allow
    removal of additional data structure used for containing
    config.xml data which are not part of manifest_x
    
    Change-Id: I56ea88500de2a5a9838326f7c432276c02d18095

--- src/widget-manifest-parser/widget_manifest_parser.cc
+++ src/widget-manifest-parser/widget_manifest_parser.cc
@@ -5,6 +5,7 @@
 
 #include "widget-manifest-parser/widget_manifest_parser.h"
 
+#include <boost/filesystem/operations.hpp>
 #include <boost/filesystem/path.hpp>
 
 #include <algorithm>
@@ -23,8 +24,6 @@
 #include "widget-manifest-parser/manifest_handlers/tizen_application_handler.h"
 #include "widget-manifest-parser/manifest_handlers/widget_handler.h"
 #include "widget-manifest-parser/permission_types.h"
-
-#define API_EXPORT __attribute__((visibility("default")))
 
 namespace bf = boost::filesystem;
 namespace keys = common_installer::application_widget_keys;
@@ -53,65 +52,139 @@ const char kWidgetIconSrcKey[] = "@src";
 
 typedef std::string LocalError;
 
-void SetError(const std::string& message, const char** error) {
-  if (error)
-    *error = strdup(message.c_str());
+void SetError(const std::string& message, std::string* error) {
+  *error = message;
 }
 
 void SetError(const std::string& message, const std::string& arg,
-    const char** error) {
-  if (error)
-    *error = strdup((message + arg).c_str());
+    std::string* error) {
+  *error = message + arg;
 }
 
-bool ExtractPackage(
-    const parser::ApplicationData& app_data, std::string* value) {
+}  // namespace
+
+namespace common_installer {
+namespace widget_manifest_parser {
+
+WidgetManifestParser::WidgetManifestParser() :
+  valid_(false) {
+}
+
+const std::string& WidgetManifestParser::GetErrorMessage() {
+  return error_;
+}
+
+bool WidgetManifestParser::FillManifestX(manifest_x* manifest) {
+  if (!valid_)
+    return false;
+  std::shared_ptr<parser::ApplicationData> app_data =
+      parser::ApplicationData::Create(bf::path(), std::string(),
+                                      parser::ApplicationData::INTERNAL,
+                                      std::move(manifest_), &error_);
+  if (!app_data.get())
+    return false;
+  parser::TizenApplicationInfo* app_info;
+  if (!(app_info = ExtractApplicationInfo(*app_data))) {
+    SetError(kErrMsgValueNotFound, "application info", &error_);
+    return false;
+  }
+  api_version_ = app_info->required_version();
+  parser::WidgetInfo* wgt_info;
+  if (!(wgt_info = ExtractWidgetInfo(*app_data))) {
+    SetError(kErrMsgValueNotFound, "widget info", &error_);
+    return false;
+  }
+  wgt_info->GetWidgetInfo()->GetString(kShortName, &short_name_);
+
+  std::vector<std::string> icons = ExtractIcons(*app_data->GetManifest(),
+                                                &error_);
+  if (!error_.empty())
+    return false;
+  std::set<std::string> privileges = ExtractPrivileges(*app_data, &error_);
+  if (!error_.empty())
+    return false;
+  // application data
+  manifest->serviceapplication = nullptr;
+  manifest->uiapplication = reinterpret_cast<uiapplication_x*>
+    (calloc (1, sizeof(uiapplication_x)));
+  manifest->uiapplication->label = reinterpret_cast<label_x*>
+    (calloc(1, sizeof(label_x)));
+  manifest->uiapplication->appcontrol = nullptr;
+
+  manifest->uiapplication->appid = strdup(app_info->id().c_str());
+  manifest->uiapplication->type = strdup("webapp");
+
+  // package data
+  manifest->package = strdup(app_info->package().c_str());
+  manifest->mainapp_id = strdup(app_info->id().c_str());
+
+  std::string version;
+  wgt_info->GetWidgetInfo()->GetString(kVersion, &version);
+  manifest->version = strdup(version.c_str());
+  manifest->type = strdup("wgt");
+
+  description_x* description = reinterpret_cast<description_x*>
+      (calloc(1, sizeof(description_x)));
+  std::string name;
+  wgt_info->GetWidgetInfo()->GetString(kName, &name);
+  description->name = strdup(name.c_str());
+  manifest->description = description;
+  manifest->label =
+      reinterpret_cast<label_x*>(calloc(1, sizeof(label_x)));
+  manifest->label->name = strdup(name.c_str());
+
+  // icons
+  for (std::string icon_str : icons) {
+    icon_x* icon = reinterpret_cast<icon_x*> (calloc(1, sizeof(icon_x)));
+    icon->name = strdup(icon_str.c_str());
+    LISTADD(manifest->icon, icon);
+  }
+
+  // privileges
+  privileges_x* privileges_x_list =
+      reinterpret_cast<privileges_x*> (calloc(1, sizeof(privileges_x)));\
+  manifest->privileges = privileges_x_list;
+  for (const std::string& p : privileges) {
+    privilege_x* privilege_x_node =
+        reinterpret_cast<privilege_x*> (calloc(1, sizeof(privilege_x)));
+    privilege_x_node->text = strdup(p.c_str());
+    LISTADD(manifest->privileges->privilege, privilege_x_node);
+  }
+
+  // For wgt package use the long name if the short name is empty
+  if (!short_name_.empty())
+    manifest->uiapplication->label->name = strdup(short_name_.c_str());
+  else
+    manifest->uiapplication->label->name =
+        strdup(manifest->description->name);
+
+  manifest->uiapplication->icon =
+      reinterpret_cast<icon_x*> (calloc(1, sizeof(icon_x)));
+  manifest->uiapplication->icon->name = strdup(manifest->icon->name);
+  manifest->uiapplication->next = nullptr;
+  return true;
+}
+
+WidgetManifestParser::~WidgetManifestParser() {
+}
+
+parser::TizenApplicationInfo* WidgetManifestParser::ExtractApplicationInfo(
+    const parser::ApplicationData& app_data) {
   parser::TizenApplicationInfo* info =
       static_cast<parser::TizenApplicationInfo*>(
           app_data.GetManifestData(keys::kTizenApplicationKey));
-  if (info)
-    *value = info->package();
   return info;
 }
 
-bool ExtractId(
-    const parser::ApplicationData& app_data, std::string* value) {
-  parser::TizenApplicationInfo* info =
-      static_cast<parser::TizenApplicationInfo*>(
-          app_data.GetManifestData(keys::kTizenApplicationKey));
-  if (info)
-    *value = info->id();
+parser::WidgetInfo* WidgetManifestParser::ExtractWidgetInfo(const ApplicationData& app_data) {
+  parser::WidgetInfo* info = static_cast<parser::WidgetInfo*>(
+      app_data.GetManifestData(keys::kWidgetKey));
   return info;
 }
 
-bool ExtractName(const parser::ApplicationData& app_data, std::string* value) {
-  parser::WidgetInfo* info = static_cast<parser::WidgetInfo*>(
-      app_data.GetManifestData(keys::kWidgetKey));
-  if (!info)
-    return false;
-  return info->GetWidgetInfo()->GetString(kName, value);
-}
-
-bool ExtractShortName(const parser::ApplicationData& app_data,
-                      std::string* value) {
-  parser::WidgetInfo* info = static_cast<parser::WidgetInfo*>(
-      app_data.GetManifestData(keys::kWidgetKey));
-  if (!info)
-    return false;
-  return info->GetWidgetInfo()->GetString(kShortName, value);
-}
-
-bool ExtractVersion(const parser::ApplicationData& app_data,
-                    std::string* value) {
-  parser::WidgetInfo* info = static_cast<parser::WidgetInfo*>(
-      app_data.GetManifestData(keys::kWidgetKey));
-  if (!info)
-    return false;
-  return info->GetWidgetInfo()->GetString(kVersion, value);
-}
-
-bool ExtractIconSrc(const common_installer::utils::Value& dict,
-    std::string* value, const char** error) {
+bool WidgetManifestParser::ExtractIconSrc(
+    const common_installer::utils::Value& dict, std::string* value,
+    std::string* error) {
   const common_installer::utils::DictionaryValue* inner_dict;
   if (!dict.GetAsDictionary(&inner_dict)) {
     SetError(kErrMsgInvalidDictionary, kWidgetIconFullKey, error);
@@ -126,173 +199,72 @@ bool ExtractIconSrc(const common_installer::utils::Value& dict,
   return  true;
 }
 
-bool ExtractIcons(const parser::Manifest& manifest,
-                  std::vector<std::string>* value, const char** error) {
+std::vector<std::string> WidgetManifestParser::ExtractIcons(
+    const parser::Manifest& manifest,
+    std::string* error) {
   common_installer::utils::Value* key_value;
+  std::vector<std::string> icons;
   if (!manifest.Get(kWidgetIconFullKey, &key_value)) {
-    value->clear();
-    return true;  // no icon, no error
+    icons.clear();
+    return icons;  // no icon, no error
   }
 
-  std::vector<std::string> icons;
   if (key_value->IsType(common_installer::utils::Value::TYPE_DICTIONARY)) {
     std::string icon;
     if (!ExtractIconSrc(*key_value, &icon, error))
-      return false;
+      return icons;
     icons.push_back(icon);
   } else if (key_value->IsType(common_installer::utils::Value::TYPE_LIST)) {
     const common_installer::utils::ListValue* list;
     if (!key_value->GetAsList(&list)) {
       SetError(kErrMsgInvalidList, kWidgetIconFullKey, error);
-      return false;
+      return icons;
     }
     for (const common_installer::utils::Value* list_value : *list) {
       std::string icon;
       if (!ExtractIconSrc(*list_value, &icon, error))
-        return false;
+        return icons;
       icons.push_back(icon);
     }
   }
-
-  value->swap(icons);
-  return true;
+  return icons;
 }
 
-bool ExtractApiVersion(const parser::ApplicationData& app_data,
-                       std::string* value) {
-  parser::TizenApplicationInfo* info =
-      static_cast<parser::TizenApplicationInfo*>(
-        app_data.GetManifestData(keys::kTizenApplicationKey));
-  if (!info)
-    return false;
-  *value = info->required_version();
-  return true;
-}
-
-bool ExtractPrivileges(const parser::ApplicationData& app_data,
-                       std::set<std::string>* value, const char** error) {
+std::set<std::string> WidgetManifestParser::ExtractPrivileges(
+    const parser::ApplicationData& app_data, std::string* error) {
   const parser::PermissionsInfo* perm_info =
        static_cast<parser::PermissionsInfo*>(
             app_data.GetManifestData(keys::kTizenPermissionsKey));
   parser::PermissionSet permissions = perm_info->GetAPIPermissions();
-  *value = permissions;
-  return true;
+  return permissions;
 }
 
-}  // namespace
-
-extern "C" {
-
-API_EXPORT bool ParseManifest(const char* path,
-    const ManifestData** data, const char** error) {
-  if (!path) {
-    SetError(kErrMsgNoPath, error);
+bool WidgetManifestParser::ParseManifest(const bf::path& manifest_path) {
+  error_.clear();
+  if (manifest_path.empty()) {
+    SetError(kErrMsgNoPath, &error_);
     return false;
   }
-  std::string str_path = path;
-  if (str_path.empty()) {
-    SetError(kErrMsgInvalidPath, error);
+  if (!exists(manifest_path)) {
+    SetError(kErrMsgInvalidPath, &error_);
     return false;
   }
 
-  std::string local_error;
-
-  std::unique_ptr<parser::Manifest> manifest = parser::LoadManifest(
-      path, parser::Manifest::TYPE_WIDGET, &local_error);
-  if (!manifest) {
-    SetError(local_error, error);
+  manifest_ = parser::LoadManifest(
+      manifest_path.string(), parser::Manifest::TYPE_WIDGET, &error_);
+  if (!manifest_)
     return false;
-  }
-
-  std::shared_ptr<parser::ApplicationData> app_data =
-      parser::ApplicationData::Create(bf::path(), std::string(),
-                                      parser::ApplicationData::INTERNAL,
-                                      std::move(manifest), &local_error);
-  if (!app_data.get()) {
-    SetError(local_error, error);
-    return false;
-  }
-
-  std::string package;
-  if (!ExtractPackage(*app_data, &package)) {
-    SetError(kErrMsgValueNotFound, "package id", error);
-    return false;
-  }
-
-  std::string id;
-  if (!ExtractId(*app_data, &id)) {
-    SetError(kErrMsgValueNotFound, "application id", error);
-    return false;
-  }
-
-  std::string name;
-  if (!ExtractName(*app_data, &name)) {
-    SetError(kErrMsgValueNotFound, "application name", error);
-    return false;
-  }
-
-  std::string short_name;
-  if (!ExtractShortName(*app_data, &short_name)) {
-    SetError(kErrMsgValueNotFound, "application short name", error);
-    return false;
-  }
-
-  std::string version;
-  if (!ExtractVersion(*app_data, &version)) {
-    SetError(kErrMsgValueNotFound, "application version", error);
-    return false;
-  }
-
-  std::vector<std::string> icons;
-  if (!ExtractIcons(*app_data->GetManifest(), &icons, error))
-    return false;
-
-  std::string api_version;
-  if (!ExtractApiVersion(*app_data, &api_version)) {
-    SetError(kErrMsgValueNotFound, "required api version", error);
-    return false;
-  }
-
-  std::set<std::string> privileges;
-  if (!ExtractPrivileges(*app_data, &privileges, error))
-    return false;
-
-  if (data) {
-    ManifestData* manifest_data = new ManifestData;
-    manifest_data->package = strdup(package.c_str());
-    manifest_data->id = strdup(id.c_str());
-    manifest_data->name = strdup(name.c_str());
-    manifest_data->short_name = strdup(short_name.c_str());
-    manifest_data->version = strdup(version.c_str());
-    manifest_data->icon =
-        icons.empty() ? strdup("") : strdup(icons.front().c_str());
-    manifest_data->api_version = strdup(api_version.c_str());
-    manifest_data->privilege_count = privileges.size();
-    manifest_data->privilege_list = new char*[privileges.size()];
-    char** privileges_it = manifest_data->privilege_list;
-    for (const std::string& p : privileges) {
-      *privileges_it = strdup(p.c_str());
-      ++privileges_it;
-    }
-    *data = manifest_data;
-  }
-
-  return true;
+  valid_ = error_.empty();
+  return valid_;
 }
 
-API_EXPORT void ReleaseData(const ManifestData* data, const char* error) {
-  free(data->package);
-  free(data->id);
-  free(data->name);
-  free(data->short_name);
-  free(data->version);
-  free(data->icon);
-  free(data->api_version);
-  for (int i = 0; i < data->privilege_count; ++i)
-    free(data->privilege_list[i]);
-  delete[] data->privilege_list;
-  delete data;
-  free((void*)error);
+const std::string& WidgetManifestParser::GetShortName() const {
+  return short_name_;
 }
 
-}  // extern "C"
+const std::string& WidgetManifestParser::GetRequiredAPIVersion() const {
+  return api_version_;
+}
+
+}  // namespace widget_manifest_parser
+}  // namespace common_installer
