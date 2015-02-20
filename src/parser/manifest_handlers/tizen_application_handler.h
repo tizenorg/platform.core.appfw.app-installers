534dad78e35a9dbd5b00165836290243468768d9
Tomasz Iwanek <t.iwanek@samsung.com>
2015-02-20 10:36:39 +0100

    Move/rename widget-manifest-parser
    Simplify naming and remove underscores in directory name.
    
    Change-Id: I3df8b3212c70e6456e8cc47c0e3f263c1ebdc514

---
+++ src/parser/manifest_handlers/tizen_application_handler.h
@@ -0,0 +1,70 @@
+// Copyright (c) 2014 Intel Corporation. All rights reserved.
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE-xwalk file.
+
+#ifndef PARSER_MANIFEST_HANDLERS_TIZEN_APPLICATION_HANDLER_H_
+#define PARSER_MANIFEST_HANDLERS_TIZEN_APPLICATION_HANDLER_H_
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include "utils/macros.h"
+#include "utils/values.h"
+#include "parser/application_manifest_constants.h"
+#include "parser/manifest_handler.h"
+
+namespace common_installer {
+namespace parser {
+
+class TizenApplicationInfo : public ApplicationData::ManifestData {
+ public:
+  TizenApplicationInfo();
+  virtual ~TizenApplicationInfo();
+
+  void set_id(const std::string& id) {
+    id_ = id;
+  }
+  void set_package(const std::string& package) {
+    package_ = package;
+  }
+  void set_required_version(
+      const std::string& required_version) {
+    required_version_ = required_version;
+  }
+  const std::string& id() const {
+    return id_;
+  }
+  const std::string& package() const {
+    return package_;
+  }
+  const std::string& required_version() const {
+    return required_version_;
+  }
+
+ private:
+  std::string id_;
+  std::string package_;
+  std::string required_version_;
+};
+
+class TizenApplicationHandler : public ManifestHandler {
+ public:
+  TizenApplicationHandler();
+  virtual ~TizenApplicationHandler();
+
+  bool Parse(std::shared_ptr<ApplicationData> application,
+             std::string* error) override;
+  bool Validate(std::shared_ptr<const ApplicationData> application,
+                std::string* error) const override;
+  std::vector<std::string> Keys() const override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(TizenApplicationHandler);
+};
+
+}  // namespace parser
+}  // namespace common_installer
+
+#endif  // PARSER_MANIFEST_HANDLERS_TIZEN_APPLICATION_HANDLER_H_
---
+++ src/parser/manifest_util.h
@@ -0,0 +1,39 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE-xwalk file.
+
+#ifndef PARSER_MANIFEST_UTIL_H_
+#define PARSER_MANIFEST_UTIL_H_
+
+#include <boost/filesystem/path.hpp>
+#include <libxml2/libxml/tree.h>
+
+#include <string>
+#include <map>
+#include <memory>
+
+#include "parser/application_data.h"
+
+// Utilities for manipulating the on-disk storage of applications.
+namespace common_installer {
+namespace parser {
+
+// Loads an application manifest from the specified directory. Returns nullptr
+// on failure, with a description of the error in |error|.
+std::unique_ptr<Manifest> LoadManifest(
+    const std::string& file_path, std::string* error);
+std::string GetNodeDir(xmlNode* node, const std::string& inherit_dir);
+std::string GetNodeText(xmlNode* root, const std::string& inherit_dir);
+bool IsPropSupportDir(xmlNode* root, xmlAttr* prop);
+bool IsElementSupportSpanAndDir(xmlNode* root);
+bool IsSingletonElement(const std::string& name);
+bool IsTrimRequiredForElement(xmlNode* root);
+bool IsTrimRequiredForProp(xmlNode* root, xmlAttr* prop);
+std::unique_ptr<utils::DictionaryValue> LoadXMLNode(
+    xmlNode* root, const std::string& inherit_dir = "");
+
+}  // namespace parser
+}  // namespace common_installer
+
+#endif  // PARSER_MANIFEST_UTIL_H_
