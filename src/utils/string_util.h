f0936e1ebc8f9b5a85e422dd1433301750a4cc48
Tomasz Iwanek <t.iwanek@samsung.com>
2015-02-20 16:16:39 +0100

    [Unitttests] string_utils_unittest
    Change-Id: I237ebe67f7f761afb5f6dfa5d6d648206e8f7d13

--- src/utils/string_util.h
+++ src/utils/string_util.h
@@ -11,6 +11,15 @@
 namespace common_installer {
 namespace utils {
 
+// utf8 representation of unicode BIDI characters
+extern const char kRightToLeftMark[];
+extern const char kLeftToRightMark[];
+extern const char kLeftToRightEmbeddingMark[];
+extern const char kRightToLeftEmbeddingMark[];
+extern const char kPopDirectionalFormatting[];
+extern const char kLeftToRightOverride[];
+extern const char kRightToLeftOverride[];
+
 std::string CollapseWhitespaceUTF8(const std::string& text);
 std::string StripWrappingBidiControlCharactersUTF8(const std::string& text);
 std::string GetDirTextUTF8(const std::string& text, const std::string& dir);
