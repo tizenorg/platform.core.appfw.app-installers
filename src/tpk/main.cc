63be8afc554e093c10464dddd92639eadee9b68f
Youmin Ha <youmin.ha@samsung.com>
2015-03-05 17:05:41 +0900

    Replace tpk manifest parser to XmlParser
    Old manifest parser is replaced with the new general-purpose XmlParser.
    With the XmlParser, the element hierarchy is fully preserved.
    In addition, all exceptions are removed in this commit.
    
    Change-Id: Id03384dc0b3c661bd876966bfbb3883b631cc68d
    Signed-Off-By: Youmin Ha <youmin.ha@samsung.com>

--- src/tpk/main.cc
+++ src/tpk/main.cc
@@ -1,19 +1,17 @@
 /* Copyright 2015 Samsung Electronics, license APACHE-2.0, see LICENSE file */
 #include <iostream>
-#include <cerrno>
 #include "tpk/task.h"
-#include "tpk/exception.h"
+#include "utils/logging.h"
 
 
 int main(const int argc, char* argv[]) {
-  try {
-    // Create a task to do
-    tpk::Task t(argc, argv);
-
-    // Do the task
-    t.Run();
-  } catch(tpk::Exception &e) {
-    e.PrintStackTrace();
+  tpk::Task t;
+  if (!t.Init(argc, argv)) {
+    std::cerr << "Task init failure" << std::endl;
+    return -1;
+  }
+  if (!t.Run()) {
+    std::cerr << "Task run failure" << std::endl;
     return -1;
   }
   return 0;
---
+++ src/xml_parser/xml_parser.cc
@@ -0,0 +1,169 @@
+/* Copyright 2015 Samsung Electronics, license APACHE-2.0, see LICENSE file */
+#include "xml_parser/xml_parser.h"
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+#include <libxml/xmlstring.h>
+#include <iostream>
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+
+using std::map;
+using std::string;
+using std::vector;
+
+namespace xml_parser {
+
+namespace {  // static namespace
+
+string xmlChar2string(const xmlChar *str) {
+  return string(const_cast<char*>(reinterpret_cast<const char*>(str)));
+}
+
+}  // namespace
+
+
+// class XmlElement
+XmlElement::XmlElement(xmlNode *node) {
+  name_ = xmlChar2string(node->name);
+  xmlChar* content = xmlNodeGetContent(node);  // NOTE: Needs to be freed
+  content_ = xmlChar2string(content);
+  xmlFree(content);
+  SetAttrMap(node);
+  node_ = node;
+}
+
+XmlElement::~XmlElement() {
+  attr_map_.clear();
+}
+
+const string& XmlElement::attr(const string& attrName) {
+  map<string, string>::iterator it = attr_map_.find(attrName);
+  if (it != attr_map_.end()) {
+    return it->second;
+  }
+  return XmlElement::null_string();
+}
+
+void XmlElement::SetAttrMap(xmlNode* node) {
+  xmlAttr* attr;
+  const xmlChar* name;
+  xmlChar* value;
+
+  for (attr=node->properties; attr != nullptr; attr=attr->next) {
+    name = attr->name;
+    value = xmlGetProp(node, name);  // NOTE: Needs to be freed
+
+    attr_map_.insert(map<string, string>::value_type(
+          xmlChar2string(name), xmlChar2string(value)));
+
+    xmlFree(value);
+  }
+}
+
+const string& XmlElement::null_string() {
+  // NOTE: check-coding-rule says that const/static string is to be changed to
+  // const char[], but here we need a string object reference, instead of
+  // C-style const char.
+  static const string nullstr = string("");
+  return nullstr;
+}
+
+
+// class XmlTree
+XmlTree::XmlTree(xmlDoc* doc) : doc_(doc), root_(nullptr) {
+}
+
+XmlTree::~XmlTree() {
+  xmlFreeDoc(doc_);
+
+  // Clear element_map_
+  for (auto& it : elements_map_) {
+    delete it.second;
+  }
+  elements_map_.clear();
+}
+
+void XmlTree::StoreElement(xmlNode* node, XmlElement *el) {
+  elements_map_.insert(std::pair<xmlNode*, XmlElement*>(node, el));
+}
+
+XmlElement* XmlTree::FindElement(xmlNode* node) {
+  map<xmlNode*, XmlElement*>::iterator it = elements_map_.find(node);
+  if (it != elements_map_.end()) {
+    return it->second;
+  }
+  return nullptr;
+}
+
+XmlElement* XmlTree::GetRootElement() {
+  if (root_) return root_;
+
+  xmlNode* root_node = xmlDocGetRootElement(doc_);
+  if (root_node == nullptr) {
+    return nullptr;
+  }
+
+  // Get information of the node
+  XmlElement* root = new XmlElement(root_node);
+  StoreElement(root_node, root);
+  root_ = root;
+
+  return root_;
+}
+
+vector<XmlElement*> XmlTree::Children(
+    XmlElement* parent,
+    const std::string& name) {
+  xmlNode* child;
+  XmlElement *el;
+  vector<XmlElement*> children;
+
+  for (child = xmlFirstElementChild(parent->node());
+      nullptr != child;
+      child = xmlNextElementSibling(child)) {
+    if (name == xmlChar2string(child->name)) {
+      // Found a matched node
+      // Search map
+      el = FindElement(child);
+      if (!el) {  // Not in elements_map_
+        el = new XmlElement(child);
+        StoreElement(child, el);
+      }
+      // Add to children
+      children.push_back(el);
+    }
+  }
+  return children;
+}
+
+
+
+// class XmlParser
+XmlParser::XmlParser() {
+}
+
+XmlParser::~XmlParser() {
+  // NOTE: These libxml2 cleanup funcs must be called after all libxml2 funcs
+  // are compelte.
+
+  // xmlCleanupParser();
+  // xmlMemoryDump();
+}
+
+XmlTree* XmlParser::ParseAndGetNewTree(const char* xmlFilePath) {
+  LIBXML_TEST_VERSION
+
+  XmlTree* t = nullptr;
+  xmlDocPtr doc = xmlReadFile(xmlFilePath, NULL, 0);
+  if (doc) {
+    t = new XmlTree(doc);
+  } else {
+    // TODO(youmin.ha@samsung.com): Error log
+  }
+  return t;
+  // NOTE: doc will be freed by XmlTree.
+}
+
+}  // namespace xml_parser
