2661d10342578ec49d74f6770e7d2d4ff5b853da
Tomasz Iwanek <t.iwanek@samsung.com>
2015-03-24 18:16:57 +0100

    [UpdateApplication] Added step for update installation
    Tizen-JIRA: TC-2482
    
    Change-Id: Ib0b2a60ba003fa81c9eaeb7c4df16fb97bdf7c21

---
+++ src/common/step/step_update_app.h
@@ -0,0 +1,30 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_STEP_STEP_UPDATE_APP_H_
+#define COMMON_STEP_STEP_UPDATE_APP_H_
+
+#include "common/context_installer.h"
+#include "common/step/step.h"
+#include "utils/logging.h"
+
+namespace common_installer {
+namespace update_app {
+
+class StepUpdateApplication : public Step {
+ public:
+  using Step::Step;
+
+  Status process() override;
+  Status clean() override;
+  Status undo() override;
+  Status precheck() override;
+
+  SCOPE_LOG_TAG(UpdateApplication)
+};
+
+}  // namespace update_app
+}  // namespace common_installer
+
+#endif  // COMMON_STEP_STEP_UPDATE_APP_H_
