d5ca6dab934b830acd4678aa489979094211d0c1
Tomasz Iwanek <t.iwanek@samsung.com>
2015-03-25 12:19:08 +0100

    [BackupIcons] Added step for update installation
    Tizen-JIRA: TC-2482
    
    Change-Id: I76870b774aac8f7cb81624de8c66a09bd8b8eee9

---
+++ src/common/step/step_backup_icons.h
@@ -0,0 +1,40 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_STEP_STEP_BACKUP_ICONS_H_
+#define COMMON_STEP_STEP_BACKUP_ICONS_H_
+
+#include <boost/filesystem/path.hpp>
+#include <utility>
+#include <vector>
+
+#include "common/context_installer.h"
+#include "common/step/step.h"
+#include "utils/logging.h"
+
+namespace common_installer {
+namespace backup_icons {
+
+class StepBackupIcons : public Step {
+ public:
+  using Step::Step;
+
+  Status process() override;
+  Status clean() override;
+  Status undo() override;
+  Status precheck() override { return Status::OK; }
+
+ private:
+  void RemoveBackupIcons();
+
+  std::vector<std::pair<boost::filesystem::path, boost::filesystem::path>>
+      icons_;
+
+  SCOPE_LOG_TAG(BackupIcons)
+};
+
+}  // namespace backup_icons
+}  // namespace common_installer
+
+#endif  // COMMON_STEP_STEP_BACKUP_ICONS_H_
