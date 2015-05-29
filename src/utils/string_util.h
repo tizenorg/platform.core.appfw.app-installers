62ee8e61bf7a651221d5f296ff324f47879bc145
Tomasz Iwanek <t.iwanek@samsung.com>
2015-05-29 16:53:17 +0200

    Reimplement signature validation with cert-svc/vcore
    This commit does not introduce any new feature in app-installer.
    Signature is reimplemented only. Prililege level check and
    other check will be implemented afterwards.
    
    Removed dependencies to openssl and xmlsec as we are using
    cert-svc now.
    
    Reworked basic tests about signature:
      "signature_unittest.cc"
    
    Removed LICENSE-xwalk as it was not used anymore. All crosswalk
    code was removed/moved to manifest-parser or rewritten.
    
    Change-Id: I59462fd0e0082f5c2ec648caedebba8874e69ddf

--- src/utils/string_util.h
+++ src/utils/string_util.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE-xwalk file.
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
 
 #ifndef UTILS_STRING_UTIL_H_
 #define UTILS_STRING_UTIL_H_
