971baf8d59712ed98eb9f720c33aed05aed7e6ff
Wojciech Kosowicz <w.kosowicz@samsung.com>
2015-06-12 17:24:29 +0200

    [XWALK-214] RDS support
    Implemented:
             * parsing rds_delta file
             * actions(add/delete/modify) on changed files
             * backup of files before modification
             * restoring files in case of failure
             * registering smack labels
    
    Change-Id: Ia0fd00567d50c70588dc6f4af69c5c3f581cc006

---
+++ src/wgt/step/step_rds_parse.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef WGT_STEP_STEP_RDS_PARSE_H_
+#define WGT_STEP_STEP_RDS_PARSE_H_
+
+#include "common/step/step.h"
+
+#include <boost/filesystem.hpp>
+#include <string>
+#include <vector>
+
+namespace wgt {
+namespace rds_parse {
+
+class StepRDSParse : public common_installer::Step {
+ public:
+  using Step::Step;
+
+  Status process() override;
+  Status clean() override { return Status::OK; }
+  Status undo() override { return Status::OK; }
+  Status precheck() override;
+
+ private:
+  boost::filesystem::path rds_file_path_;
+};
+}  // namespace rds_parse
+}  // namespace wgt
+#endif  // WGT_STEP_STEP_RDS_PARSE_H_
