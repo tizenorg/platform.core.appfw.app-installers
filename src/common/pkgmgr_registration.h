c5256a1ac154957f561dd59db4cb4ea18d009839
Tomasz Iwanek <t.iwanek@samsung.com>
2015-06-12 11:01:12 +0200

    Save author certificate to pkgmgr database
    Registration and unregistation of package was moved to
    pkgmgr_registration.cc.
    
    Change-Id: I80c838983b16302b7bea1d2acfe4d271e8732901

---
+++ src/common/pkgmgr_registration.h
@@ -0,0 +1,29 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_PKGMGR_REGISTRATION_H_
+#define COMMON_PKGMGR_REGISTRATION_H_
+
+#include <boost/filesystem.hpp>
+#include <unistd.h>
+
+#include <string>
+
+#include "common/context_installer.h"
+
+namespace common_installer {
+
+bool RegisterAppInPkgmgr(const boost::filesystem::path& xml_path,
+                         const std::string& pkgid,
+                         const CertificateInfo& cert_info, uid_t uid);
+bool UpgradeAppInPkgmgr(const boost::filesystem::path& xml_path,
+                        const std::string& pkgid,
+                        const CertificateInfo& cert_info, uid_t uid);
+bool UnregisterAppInPkgmgr(const boost::filesystem::path& xml_path,
+                           const std::string& pkgid, uid_t uid);
+std::string QueryCertificateAuthorCertificate(const std::string& pkgid);
+
+}  // namespace common_installer
+
+#endif  // COMMON_PKGMGR_REGISTRATION_H_
