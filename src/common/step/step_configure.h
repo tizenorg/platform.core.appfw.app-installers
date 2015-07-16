e045006565388792b442b8e43bcae73f79bc89ec
Tomasz Iwanek <t.iwanek@samsung.com>
2015-07-16 11:11:58 +0200

    Forbid installation for superuser
    App-installers binaries should be run with
    normal user or tizenglobalapp.
    
    Change-Id: I0b13457e6760678fe21843a60bb98f035d5f50f8

--- src/common/step/step_configure.h
+++ src/common/step/step_configure.h
@@ -20,7 +20,7 @@ class StepConfigure : public Step {
   Status process() override;
   Status clean() override { return Status::OK; }
   Status undo() override { return Status::OK; }
-  Status precheck() override { return Status::OK; }
+  Status precheck() override;
  private:
   bool SetupRootAppDirectory();
 
