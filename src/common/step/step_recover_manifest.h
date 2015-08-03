6c6a7cc7dacdc80a415e7a080a5447acfe4cd5f8
Tomasz Iwanek <t.iwanek@samsung.com>
2015-08-03 15:56:15 +0200

    [Recovery] StepRecoverManifest
    Change-Id: I21c480d113ddf6211ad25298c5753f9f5f57f71a

---
+++ src/common/step/step_recover_manifest.h
@@ -0,0 +1,39 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache-2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_STEP_STEP_RECOVER_MANIFEST_H_
+#define COMMON_STEP_STEP_RECOVER_MANIFEST_H_
+
+#include "common/context_installer.h"
+#include "common/step/step_recovery.h"
+#include "common/utils/logging.h"
+
+namespace common_installer {
+namespace filesystem {
+
+/**
+ * @brief The StepRecoverManifest class
+ *        Fixes state of platform manfiest file in recovery mode.
+ *
+ * For recovering new installation, manifest file is removed.
+ * For recovering update installation, the old manifest of package is restored
+ * to its location.
+ */
+class StepRecoverManifest : public recovery::StepRecovery {
+ public:
+  using StepRecovery::StepRecovery;
+
+  Status RecoveryNew() override;
+  Status RecoveryUpdate() override;
+
+ private:
+  bool SetXmlPaths();
+
+  SCOPE_LOG_TAG(RecoverManifest)
+};
+
+}  // namespace filesystem
+}  // namespace common_installer
+
+#endif  // COMMON_STEP_STEP_RECOVER_MANIFEST_H_
