bbbb2312a069dfa37ddb8ada657d99e1e7ca212a
Tomasz Iwanek <t.iwanek@samsung.com>
2015-08-03 16:11:30 +0200

    [Recovery] StepRecoverIcons
    For recovering new installation, any existing icons are removed.
    For recovering update installation, all icons are moved to
    original locations.
    
    Change-Id: I80e34748a4e753450d8a4cbdf8f81dbbeb5b44ae

---
+++ src/common/step/step_recover_icons.cc
@@ -0,0 +1,73 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache-2.0 license that can be
+// found in the LICENSE file.
+
+#include "common/step/step_recover_icons.h"
+
+#include <boost/filesystem/operations.hpp>
+#include <boost/system/error_code.hpp>
+#include <pkgmgr-info.h>
+
+#include "common/utils/clist_helpers.h"
+#include "common/utils/file_util.h"
+
+namespace bf = boost::filesystem;
+namespace bs = boost::system;
+
+namespace common_installer {
+namespace filesystem {
+
+Step::Status StepRecoverIcons::RecoveryNew() {
+  if (!TryGatherIcons()) {
+    LOG(DEBUG) << "Icons recovery not needed";
+    return Status::OK;
+  }
+  for (auto& pair : icons_) {
+    bs::error_code error;
+    bf::remove(pair.first, error);
+  }
+  LOG(INFO) << "Icons recovery done";
+  return Status::OK;
+}
+
+Step::Status StepRecoverIcons::RecoveryUpdate() {
+  if (!TryGatherIcons()) {
+    LOG(DEBUG) << "Icons recovery not needed";
+    return Status::OK;
+  }
+  for (auto& pair : icons_) {
+    if (bf::exists(pair.first)) {
+      bs::error_code error;
+      bf::remove(pair.first, error);
+      if (error) {
+        LOG(ERROR) << "Cannot move icon to restore its correct location";
+        return Status::ERROR;
+      }
+    }
+    (void) MoveFile(pair.second, pair.first);
+  }
+  LOG(INFO) << "Icons recovery done";
+  return Status::OK;
+}
+
+bool StepRecoverIcons::TryGatherIcons() {
+  if (!context_->manifest_data.get())
+    return false;
+  uiapplication_x* ui = nullptr;
+  PKGMGR_LIST_MOVE_NODE_TO_HEAD(context_->manifest_data.get()->uiapplication,
+                                ui);
+  for (; ui != nullptr; ui = ui->next) {
+    bf::path app_icon = bf::path(getIconPath(context_->uid.get()))
+      / bf::path(ui->appid);
+    app_icon += ".png";
+    bf::path icon_backup = app_icon;
+    icon_backup += ".bck";
+    if (bf::exists(icon_backup) || bf::exists(app_icon))
+        icons_.emplace_back(app_icon, icon_backup);
+  }
+  return true;
+}
+
+}  // namespace filesystem
+}  // namespace common_installer
+
