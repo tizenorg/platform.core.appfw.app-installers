acc4e9b0eed30272df08c70b4ae05f31767c8185
Tomasz Iwanek <t.iwanek@samsung.com>
2015-08-03 16:11:30 +0200

    [Recovery] StepRecoverIcons
    For recovering new installation, any existing icons are removed.
    For recovering update installation, all icons are moved to
    original locations.
    
    Change-Id: I80e34748a4e753450d8a4cbdf8f81dbbeb5b44ae

---
+++ src/common/step/step_recover_icons.h
@@ -0,0 +1,46 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache-2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_STEP_STEP_RECOVER_ICONS_H_
+#define COMMON_STEP_STEP_RECOVER_ICONS_H_
+
+#include <boost/filesystem/path.hpp>
+#include <utility>
+#include <vector>
+
+#include "common/context_installer.h"
+#include "common/step/step_recovery.h"
+#include "common/utils/logging.h"
+
+namespace common_installer {
+namespace filesystem {
+
+/**
+ * @brief The StepRecoverIcons class
+ *        Fixes state of platform icon files in recovery mode.
+ *
+ * For recovery of new installation, all icons files are removed.
+ * For recovery of update installation, all icons of applications of package are
+ * restored to its previous locations.
+ */
+class StepRecoverIcons : public recovery::StepRecovery {
+ public:
+  using StepRecovery::StepRecovery;
+
+  Status RecoveryNew() override;
+  Status RecoveryUpdate() override;
+
+ private:
+  bool TryGatherIcons();
+
+  std::vector<std::pair<boost::filesystem::path, boost::filesystem::path>>
+      icons_;
+
+  SCOPE_LOG_TAG(RecoverIcons)
+};
+
+}  // namespace filesystem
+}  // namespace common_installer
+
+#endif  // COMMON_STEP_STEP_RECOVER_ICONS_H_
