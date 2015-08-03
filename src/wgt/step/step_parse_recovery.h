66418ad1dcb35709a5c9ef7b74c98a8a61abd790
Tomasz Iwanek <t.iwanek@samsung.com>
2015-08-03 09:39:36 +0200

    [Recovery] StepParseRecovery
    Change-Id: I4dbe7a22a11d282e35e9c426404a474157663a2e

---
+++ src/wgt/step/step_parse_recovery.h
@@ -0,0 +1,39 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache-2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef WGT_STEP_STEP_PARSE_RECOVERY_H_
+#define WGT_STEP_STEP_PARSE_RECOVERY_H_
+
+#include "common/utils/logging.h"
+#include "wgt/step/step_parse.h"
+
+namespace wgt {
+namespace parse {
+/**
+ * @brief The StepParseRecovery class
+ *        Retrievies package information from config.xml during RECOVERY.
+ *
+ * Step is used in recovery mode.
+ *
+ * Parse config.xml file by guessing possible locations:
+ *  - backup package directory
+ *  - package installation directory
+ * to get information about widget package to be recovered.
+ *
+ */
+class StepParseRecovery : public StepParse {
+ public:
+  using StepParse::StepParse;
+
+  Status process() override;
+  Status precheck() override;
+  bool LocateConfigFile() override;
+
+  SCOPE_LOG_TAG(ParseRecovery)
+};
+
+}  // namespace parse
+}  // namespace wgt
+
+#endif  // WGT_STEP_STEP_PARSE_RECOVERY_H_
