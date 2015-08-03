eeabc3bf71b20ce705879574cec600a8d866e99f
Tomasz Iwanek <t.iwanek@samsung.com>
2015-08-03 15:56:15 +0200

    [Recovery] StepRecoverManifest
    Change-Id: I21c480d113ddf6211ad25298c5753f9f5f57f71a

---
+++ src/common/step/step_recover_manifest.cc
@@ -0,0 +1,66 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache-2.0 license that can be
+// found in the LICENSE file.
+
+#include "common/step/step_recover_manifest.h"
+
+#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/system/error_code.hpp>
+#include <pkgmgr-info.h>
+
+#include "common/utils/file_util.h"
+
+namespace bf = boost::filesystem;
+namespace bs = boost::system;
+
+namespace common_installer {
+namespace filesystem {
+
+Step::Status StepRecoverManifest::RecoveryNew() {
+  if (!SetXmlPaths()) {
+    LOG(DEBUG) << "Manifest recovery not needed";
+    return Status::OK;
+  }
+  if (bf::exists(context_->xml_path.get())) {
+    bs::error_code error;
+    bf::remove(context_->xml_path.get(), error);
+  }
+  LOG(INFO) << "Manifest recovery done";
+  return Status::OK;
+}
+
+Step::Status StepRecoverManifest::RecoveryUpdate() {
+  if (!SetXmlPaths()) {
+    LOG(DEBUG) << "Manifest recovery not needed";
+    return Status::OK;
+  }
+  if (bf::exists(context_->backup_xml_path.get())) {
+    if (bf::exists(context_->xml_path.get())) {
+      bs::error_code error;
+      bf::remove(context_->xml_path.get(), error);
+      if (error) {
+        LOG(ERROR) << "Cannot move manifest file to restore its location";
+        return Status::ERROR;
+      }
+    }
+    (void) MoveFile(context_->backup_xml_path.get(), context_->xml_path.get());
+  }
+  LOG(INFO) << "Manifest recovery done";
+  return Status::OK;
+}
+
+bool StepRecoverManifest::SetXmlPaths() {
+  if (context_->pkgid.get().empty())
+    return false;
+  bf::path xml_path = bf::path(getUserManifestPath(context_->uid.get()))
+      / context_->pkgid.get();
+  xml_path += ".xml";
+  context_->xml_path.set(xml_path);
+  xml_path += ".bck";
+  context_->backup_xml_path.set(xml_path);
+  return true;
+}
+
+}  // namespace filesystem
+}  // namespace common_installer
