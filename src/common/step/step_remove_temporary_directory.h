719379b302c47734a8f8c763ac026d4f4a9c758a
Wojciech Kosowicz <w.kosowicz@samsung.com>
2015-08-03 11:45:25 +0200

    [Recovery] StepRemoveTemporaryDirectory
    Step deletes all the directories that starts with 'unpack' within
    root app directory
    
    Change-Id: I22c5b8a3db9fc370876a47a8193a8f5490b48fb9

---
+++ src/common/step/step_remove_temporary_directory.h
@@ -0,0 +1,39 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_STEP_STEP_REMOVE_TEMPORARY_DIRECTORY_H_
+#define COMMON_STEP_STEP_REMOVE_TEMPORARY_DIRECTORY_H_
+
+#include "common/step/step_recovery.h"
+
+namespace common_installer {
+namespace filesystem {
+
+/**
+ * @brief The StepRemoveTemporaryDirectory class
+ *
+ * Part of Recovery Mode. If recovery mode gets called
+ * path to temporary unpack directory and its content get
+ * deleted
+ */
+class StepRemoveTemporaryDirectory : public recovery::StepRecovery {
+ public:
+  using StepRecovery::StepRecovery;
+
+  Status RecoveryNew() override;
+  Status RecoveryUpdate() override;
+ private:
+  /**
+   * @brief RemoveFiles
+   * Removes all the temporary files
+   */
+  void RemoveFiles();
+
+  SCOPE_LOG_TAG(RemoveTemporaryDirectory)
+};
+
+}  // namespace filesystem
+}  // namespace common_installer
+
+#endif  // COMMON_STEP_STEP_REMOVE_TEMPORARY_DIRECTORY_H_
