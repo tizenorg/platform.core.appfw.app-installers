7ee75a8c8316c65e13d55c3e6819a03649d7342c
Tomasz Iwanek <t.iwanek@samsung.com>
2015-08-10 13:14:16 +0200

    Fix preserving icon extension
    Code of:
     - StepBackupIcons,
     - StepRecoverIcons,
     - StepRemoveIcons,
    was adjusted to code of StepCreateIcons which preserves
    icon extension during creation of platform icons.
    
    Note: internalizalizaton of icons is not supported as
    there is needed policy so there is copy of one icon per
    ui application of package.
    
    Change-Id: Ic9229a64d0a4973f6ab3275c12dd9ca4672a73a3

--- src/common/step/step_backup_icons.cc
+++ src/common/step/step_backup_icons.cc
@@ -24,8 +24,11 @@ Step::Status StepBackupIcons::process() {
   // gather icon info
   for (; ui != nullptr; ui = ui->next) {
     bf::path app_icon = bf::path(getIconPath(context_->uid.get()))
-      / bf::path(ui->appid);
-    app_icon += ".png";
+        / bf::path(ui->appid);
+    if (ui->icon && ui->icon->name)
+      app_icon += bf::path(ui->icon->name).extension();
+    else
+      app_icon += ".png";
     bf::path icon_backup = GetBackupPathForIconFile(app_icon);
     if (bf::exists(app_icon))
         icons_.emplace_back(app_icon, icon_backup);
