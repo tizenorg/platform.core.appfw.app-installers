70a696057b4101ed806872e3e15fd68e9fe8d4b4
Tomasz Iwanek <t.iwanek@samsung.com>
2015-08-11 10:28:21 +0200

    Add static tpk backend library
    For linking with testsuites.
    Task was renamed to TpkInstaller class.
    
    HOSTTEST was removed due to missing files for this configuration
    in project files.
    
    Change-Id: I97de47bb1b4a622cfea89c8f7a692253056a1535

---
+++ src/tpk/tpk_installer.h
@@ -0,0 +1,37 @@
+/* Copyright 2015 Samsung Electronics, license APACHE-2.0, see LICENSE file */
+
+#ifndef TPK_TPK_INSTALLER_H_
+#define TPK_TPK_INSTALLER_H_
+
+#include "common/app_installer.h"
+#include "common/pkgmgr_interface.h"
+#include "common/utils/logging.h"
+
+namespace tpk {
+
+/**
+ * @brief The TpkInstaller class
+ *        Handles request of tpk packages.
+ *
+ * This class is main class for installation/update/deinstallation of tpk
+ * packages. Pkgmgr request is parsed within and sequence of steps is built to
+ * be run.
+ */
+class TpkInstaller : public common_installer::AppInstaller {
+ public:
+  TpkInstaller(common_installer::PkgMgrPtr pkgmgr);
+  ~TpkInstaller();
+  void Prepare();
+
+ private:
+  void InstallSteps();
+  void UpdateSteps();
+  void UninstallSteps();
+  void ReinstallSteps();
+
+  SCOPE_LOG_TAG(TpkInstaller)
+};
+
+}  // namespace tpk
+
+#endif  // TPK_TPK_INSTALLER_H_
