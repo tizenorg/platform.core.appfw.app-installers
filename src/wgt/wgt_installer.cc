7fa4e3db2fa0f1b741f2f4fda11e825fb12923cd
Pawel Sikorski <p.sikorski@samsung.com>
2015-08-13 12:41:12 +0200

    WebApp encryption step alignement to libwebapenc library API change
    Requires: https://review.tizen.org/gerrit/#/c/46007/
    
    Change-Id: I718f11f597cf8a7bd9916607864920582ca21cd7

--- src/wgt/wgt_installer.cc
+++ src/wgt/wgt_installer.cc
@@ -37,6 +37,7 @@
 
 #include "wgt/step/step_create_symbolic_link.h"
 #include "wgt/step/step_check_settings_level.h"
+#include "wgt/step/step_encrypt_resources.h"
 #include "wgt/step/step_parse.h"
 #include "wgt/step/step_parse_recovery.h"
 #include "wgt/step/step_remove_encryption_data.h"
@@ -61,6 +62,7 @@ WgtInstaller::WgtInstaller() : AppInstaller("wgt") {
       AddStep<wgt::parse::StepParse>();
       AddStep<ci::security::StepCheckSignature>();
       AddStep<wgt::security::StepCheckSettingsLevel>();
+      AddStep<wgt::encrypt::StepEncryptResources>();
       AddStep<wgt::filesystem::StepWgtResourceDirectory>();
       AddStep<ci::filesystem::StepCopy>();
       AddStep<wgt::filesystem::StepWgtCreateStorageDirectories>();
