05cc8958c347d283936c44cc78a9408c9325d48f
Maciej Socha <m.socha@samsung.com>
2015-08-20 14:25:48 +0200

    [Feature] Integration step_parse for TPK tizen_manifest.xml file with manifest_parser
    Change-Id: I109956932b81e8af4fe2cbe54b5525292754555f

--- src/tpk/step/step_parse.h
+++ src/tpk/step/step_parse.h
@@ -1,10 +1,22 @@
-/* Copyright 2015 Samsung Electronics, license APACHE-2.0, see LICENSE file */
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache 2.0 license that can be
+// found in the LICENSE file.
+
 #ifndef TPK_STEP_STEP_PARSE_H_
 #define TPK_STEP_STEP_PARSE_H_
 
+#include <boost/filesystem.hpp>
+#include <tpk_manifest_handlers/privileges_handler.h>
+#include <tpk_manifest_handlers/tpk_config_parser.h>
+
+#include <memory>
+#include <set>
+#include <string>
+
+#include "common/app_installer.h"
+#include "common/context_installer.h"
 #include "common/step/step.h"
 #include "common/utils/logging.h"
-#include "tpk/xml_parser/xml_parser.h"
 
 namespace tpk {
 namespace parse {
@@ -12,21 +24,40 @@ namespace parse {
 class StepParse : public common_installer::Step {
  public:
   using Step::Step;
+  // explicit StepParse(common_installer::ContextInstaller* context,
+  //     bool check_start_file);
 
-  Status process()  override;
-  Status clean()    override { return Status::OK; };
-  Status undo()     override { return Status::OK; };
-  Status precheck() override;
+  Status process() override { return Status::OK; }
+  Status clean() override { return Status::OK; }
+  Status undo() override { return Status::OK; }
+  Status precheck() override { return Status::OK; }
 
  protected:
+  // virtual bool LocateConfigFile();
+  bool Check(const boost::filesystem::path& widget_path);
+  // required by step_recovery
   virtual boost::filesystem::path LocateConfigFile() const;
 
+  boost::filesystem::path config_;
+
  private:
-  bool SetContextByManifestParser(xml_parser::XmlTree* tree);
-  bool SetPkgInfoManifest(manifest_x* m,
-      xml_parser::XmlTree* tree, xml_parser::XmlElement* manifest);
-  bool SetPkgInfoChildren(manifest_x *m,
-      xml_parser::XmlTree *tree, xml_parser::XmlElement* manifest);
+  std::set<std::string> ExtractPrivileges(
+      std::shared_ptr<const PrivilegesInfo> perm_info) const;
+
+  const std::string& GetPackageVersion(const std::string& manifest_version);
+
+  bool FillIconPaths(manifest_x* manifest);
+  bool FillApplicationInfo(manifest_x* manifest);
+  bool FillAuthorInfo(manifest_x* manifest);
+  bool FillDescription(manifest_x* manifest);
+  bool FillAppControl(manifest_x* manifest);
+  bool FillDataControl(manifest_x* manifest);
+  bool FillPrivileges(manifest_x* manifest);
+  bool FillMetadata(manifest_x* manifest);
+  // bool FillManifestX(manifest_x* manifest);
+
+  std::unique_ptr<tpk::parse::TPKConfigParser> parser_;
+  bool check_start_file_;
 
   SCOPE_LOG_TAG(Parse)
 };
