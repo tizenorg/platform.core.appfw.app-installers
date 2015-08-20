8ee7a1d262ff5ea494848fe332c7a26c2845d510
Tomasz Iwanek <t.iwanek@samsung.com>
2015-08-20 09:24:23 +0200

    [Bug] Fix installation/deinstallation mode with smack
    As stated in: https://review.tizen.org/gerrit/#/c/46420/
    
    """
    On uninstall steps, old code runs StepRevokeSecurity before
    StepRemoveFiles. With SMACK enabled, once StepRevokeSecurity
    is finished, the installer processes(tpk and wgt) don't have
    an authority to remove files, so the app
    directory($HOME/apps_rw/[pkgid]) is still remained. When the
    same app is installed again, the installer process crashes,
    as the boost::exists() to the app destination directory raises
    an exception on SMACK denial. This patch moves the
    StepRevokeSecurity to the end of the list of steps on uninstall,
    to give the privilege to delete the app directory to the installers.
    """
    
    Splitting StepRevokeSecurity into two parts so that deinstallation
    and deinstallation rollback should work.
    
    Splitting StepRegisterSecurity into 2 steps. Because we need to
    remove files before revoking security on rollback.
    
    Change-Id: If597a3c256e09d57aa923566f5c3454d497c0593

---
+++ src/common/step/step_rollback_deinstallation_security.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache-2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_STEP_STEP_ROLLBACK_DEINSTALLATION_SECURITY_H_
+#define COMMON_STEP_STEP_ROLLBACK_DEINSTALLATION_SECURITY_H_
+
+#include "common/step/step.h"
+#include "common/utils/logging.h"
+
+namespace common_installer {
+namespace security {
+
+// Step that is used during uninstallation to rollback security changes if
+// deinstallation fails
+class StepRollbackDeinstallationSecurity : public Step {
+ public:
+  using Step::Step;
+
+  Status process() override { return Status::OK; }
+  Status undo() override;
+  Status clean() override { return Status::OK; }
+  Status precheck() override;
+
+  SCOPE_LOG_TAG(RollbackDeinstallationSecurity)
+};
+
+}  // namespace security
+}  // namespace common_installer
+
+#endif  // COMMON_STEP_STEP_ROLLBACK_DEINSTALLATION_SECURITY_H_
