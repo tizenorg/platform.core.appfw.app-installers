aef68a8634f560b33d1c6cd464f7d0c41857dd5c
Tomasz Iwanek <t.iwanek@samsung.com>
2015-10-02 11:01:32 +0200

    Package storage abstraction - external installation
    This patch removes pkg_path of context and adds abstraction
    for package directory. Subclass of PackageStorage must be created
    before any access to installation location of package can be performed.
    
    Any class that implements abstract class PackageStorage must supply:
     - initialication method,
     - package path getter function (were filesystem is prepared and mounted),
     - commit/abort methods to take action when installation was successful/failed.
    
    Following patchsets should be submitted together:
     - https://review.tizen.org/gerrit/61678
     - https://review.tizen.org/gerrit/61679
     - https://review.tizen.org/gerrit/61680
    
    Verify with other commits.
    
    Change-Id: I3c49eaa3c65ce318b2e1ce6ccf049d285a83e58b

---
+++ src/common/package_external_storage.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache-2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_PACKAGE_EXTERNAL_STORAGE_H_
+#define COMMON_PACKAGE_EXTERNAL_STORAGE_H_
+
+#include <app2ext_interface.h>
+#include <boost/filesystem/path.hpp>
+
+#include <string>
+
+#include "common/package_storage.h"
+
+namespace common_installer {
+
+class PackageExternalStorage : public PackageStorage {
+ public:
+  PackageExternalStorage(RequestType type, const std::string& pkgid,
+                         const boost::filesystem::path& application_root);
+  ~PackageExternalStorage() override;
+  bool Initialize(const boost::filesystem::path& space_requirement) override;
+  bool IsRequestedPackageInstalled() const override;
+  bool Commit() override;
+  bool Abort() override;
+  const std::string& StorageName() const override;
+
+ private:
+  bool Finalize(bool success);
+
+  app2ext_handle* handle_;
+};
+
+}  // namespace common_installer
+
+#endif  // COMMON_PACKAGE_EXTERNAL_STORAGE_H_
---
+++ src/common/package_storage.cc
@@ -0,0 +1,92 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache-2.0 license that can be
+// found in the LICENSE file.
+
+#include "common/package_storage.h"
+
+#include <boost/filesystem/operations.hpp>
+#include <boost/system/error_code.hpp>
+
+#include <manifest_parser/utils/logging.h>
+
+#include <cstring>
+#include <list>
+
+#include "common/package_external_storage.h"
+#include "common/package_internal_storage.h"
+
+namespace bf = boost::filesystem;
+namespace bs = boost::system;
+
+namespace {
+
+const char kPreferExternal[] = "prefer-external";
+const char kInternalOnly[] = "internal-only";
+
+}
+
+namespace common_installer {
+
+boost::filesystem::path PackageStorage::path() const {
+  return application_root_ / pkgid_;
+}
+
+// Implements policy of acquiring package location
+std::unique_ptr<PackageStorage> AcquirePackageStorage(RequestType type,
+    const boost::filesystem::path& application_root, const std::string& pkgid,
+    const boost::filesystem::path& space_requirement,
+    const char* installed_storage, const char* preference) {
+  LOG(INFO) << "Acquiring package storage with preference: '" << preference
+            << "' and current installation: '"
+            << (installed_storage ?
+                std::string(installed_storage) : std::string("none")) << "'";
+  std::unique_ptr<PackageStorage> external_storage(
+      new PackageExternalStorage(type, pkgid, application_root));
+  std::unique_ptr<PackageStorage> internal_storage(
+      new PackageInternalStorage(type, pkgid, application_root));
+
+  // if installed
+  if (installed_storage) {
+    if (external_storage->StorageName() == installed_storage) {
+      if (!external_storage->Initialize(space_requirement)) {
+        LOG(ERROR) << "Initialization of external storage failed";
+        return nullptr;
+      }
+      return external_storage;
+    } else if (internal_storage->StorageName() == installed_storage) {
+      if (!internal_storage->Initialize(space_requirement)) {
+        LOG(ERROR) << "Initialization of internal storage failed";
+        return nullptr;
+      }
+      return internal_storage;
+    } else {
+      return nullptr;
+    }
+  }
+
+  // if not installed
+  if (preference) {
+    if (strcmp(preference, kPreferExternal) == 0) {
+      if (external_storage->Initialize(space_requirement)) {
+        return external_storage;
+      }
+      LOG(WARNING) << "Initialization of external storage failed";
+    }
+    if (!internal_storage->Initialize(space_requirement)) {
+      if (strcmp(preference, kInternalOnly) == 0) {
+        LOG(ERROR) << "Initialization of internal storage failed";
+        return nullptr;
+      }
+      LOG(WARNING) << "Initialization of internal storage failed";
+      if (external_storage->Initialize(space_requirement)) {
+        return external_storage;
+      }
+      LOG(WARNING) << "Initialization of external storage failed";
+    }
+    return internal_storage;
+  }
+
+  return nullptr;
+}
+
+}  // namespace common_installer
