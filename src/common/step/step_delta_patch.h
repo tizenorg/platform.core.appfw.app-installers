5e9e8d748816a5f723cc3db0366a9ef1e0ee2093
Tomasz Iwanek <t.iwanek@samsung.com>
2015-10-27 11:50:59 +0100

    Add RequestType::Delta and implement delta mode for tpk
    Requires:
     - submission of https://review.tizen.org/gerrit/#/c/50418/
     - release of xdelta3 for tizen 3.0
    
    Change-Id: I3066c9457368e9ab229d1500b0cd4a7549edce15

---
+++ src/common/step/step_delta_patch.h
@@ -0,0 +1,39 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache-2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_STEP_STEP_DELTA_PATCH_H_
+#define COMMON_STEP_STEP_DELTA_PATCH_H_
+
+#include "common/installer_context.h"
+
+#include "common/step/step.h"
+#include "common/utils/logging.h"
+
+namespace common_installer {
+namespace filesystem {
+
+/**
+ * @brief The StepDeltaPatch class
+ *        Patches the unpack directory content according to delta file so that
+ *        new package content is complete and no file is missing.
+ */
+class StepDeltaPatch : public Step {
+ public:
+  using Step::Step;
+
+  Status process() override;
+  Status clean() override { return Status::OK; }
+  Status undo() override;
+  Status precheck() override;
+
+ private:
+  boost::filesystem::path patch_dir_;
+
+  SCOPE_LOG_TAG(DeltaPatch)
+};
+
+}  // namespace filesystem
+}  // namespace common_installer
+
+#endif  // COMMON_STEP_STEP_DELTA_PATCH_H_
