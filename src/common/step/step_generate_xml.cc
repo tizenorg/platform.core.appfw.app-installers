c328ed26c2a6915caf292ecabab498daaf4364df
Lukasz Wysocki <l.wysocki@samsung.com>
2015-10-29 11:19:16 +0100

    Add handling for background-category element.
    This commit depend on:
     - https://review.tizen.org/gerrit/#/c/50500/
     - https://review.tizen.org/gerrit/#/c/52917/
    
    changes should be submitted together.
    
    Created sperated header files for classes that
    belong to ExtraManifestData.
    
    Change-Id: Ia487d33b9c83977852a292256499077cf805ee83

--- src/common/step/step_generate_xml.cc
+++ src/common/step/step_generate_xml.cc
@@ -170,6 +170,14 @@ common_installer::Step::Status StepGenerateXml::GenerateApplicationCommonXml(
     xmlTextWriterEndElement(writer);
   }
 
+  for (const char* background_category : GListRange<char*>(
+      app->background_category)) {
+    xmlTextWriterStartElement(writer, BAD_CAST "background-category");
+    xmlTextWriterWriteAttribute(writer, BAD_CAST "value",
+        BAD_CAST background_category);
+    xmlTextWriterEndElement(writer);
+  }
+
   return Step::Status::OK;
 }
 
