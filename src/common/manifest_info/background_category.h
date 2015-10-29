8791e31f5e21a7f6259b5e5bbca0c6edaffc2643
Lukasz Wysocki <l.wysocki@samsung.com>
2015-10-29 11:19:16 +0100

    Added handling for background-category element.
    This commit depend on:
     - https://review.tizen.org/gerrit/#/c/50500/
    
    changes should be submitted together.
    
    Created sperated header files for classes that
    belong to ExtraManifestData.
    
    Change-Id: Ia487d33b9c83977852a292256499077cf805ee83

---
+++ src/common/manifest_info/background_category.h
@@ -0,0 +1,59 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE-xwalk file.
+
+#ifndef SRC_COMMON_MANIFEST_INFO_BACKGROUND_CATEGORY_H_
+#define SRC_COMMON_MANIFEST_INFO_BACKGROUND_CATEGORY_H_
+
+#include <vector>
+#include <string>
+
+namespace common_installer {
+
+/**
+ * \brief Details about background-category
+ */
+struct BackgroundCategoryInfo {
+  std::string appid;
+  std::string providerid;
+
+  // Data members
+  std::string value;
+};
+
+/**
+ * \brief Contains all background-category instances
+ */
+class BackgroundCategoryContainer {
+ public:
+  /**
+   * \brief Default constructor
+   */
+  BackgroundCategoryContainer() {}
+
+  /**
+   * \brief Accessor method for background categories details
+   *
+   * \return Reference for vector of BackgroundCategoryInfo
+   */
+  const std::vector<BackgroundCategoryInfo>& background_categories() const {
+    return background_categories_;
+  }
+
+  /**
+   * \brief Adds BackgroundCategoryInfo object to the collection
+   *
+   * \param background_category details about background-category element
+   */
+  void add_background_category(
+      const BackgroundCategoryInfo& background_category) {
+    background_categories_.push_back(background_category);
+  }
+
+ private:
+  std::vector<BackgroundCategoryInfo> background_categories_;
+};
+
+} // namespace common_installer
+
+#endif /* SRC_COMMON_MANIFEST_INFO_BACKGROUND_CATEGORY_H_ */
