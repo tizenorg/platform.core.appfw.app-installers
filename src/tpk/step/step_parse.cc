823f0462aa057762e235ffc3360a9df04918ee28
Tomasz Iwanek <t.iwanek@samsung.com>
2015-11-17 10:57:59 +0100

    Pass manifest_x directly to pkgmgr_parser API
    manifest_x structure is prepared inside app-installers and
    passed to API (according to changes) to prevent extra parsing
    of xml file.
    
    Each lang member is now assigned with value of DEFAULT_LOCALE macro as
    pkgmgr-info expects this.
    
    Application exec attribute was fixed to contain full path.
    
    Requires submission together with: https://review.tizen.org/gerrit/51972
    
    Change-Id: I480d1905b4e885756f8140210e56f8fcca58f455

--- src/tpk/step/step_parse.cc
+++ src/tpk/step/step_parse.cc
@@ -17,6 +17,7 @@
 
 #include <pkgmgr/pkgmgr_parser.h>
 
+#include <chrono>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
@@ -40,6 +41,7 @@ namespace app_keys = tpk::application_keys;
 namespace bf = boost::filesystem;
 
 const char kManifestFileName[] = "tizen-manifest.xml";
+const char kTizenPackageXmlNamespace[] = "http://tizen.org/ns/packages";
 
 }  // namepsace
 
@@ -86,6 +88,15 @@ bf::path StepParse::LocateConfigFile() const {
   return path;
 }
 
+bool StepParse::FillInstallationInfo(manifest_x* manifest) {
+  manifest->root_path = strdup(
+      (context_->root_application_path.get() / manifest->package).c_str());
+  manifest->installed_time =
+      strdup(std::to_string(std::chrono::system_clock::to_time_t(
+          std::chrono::system_clock::now())).c_str());
+  return true;
+}
+
 bool StepParse::FillPackageInfo(manifest_x* manifest) {
   std::shared_ptr<const PackageInfo> app_info =
       std::static_pointer_cast<const PackageInfo>(
@@ -109,8 +120,12 @@ bool StepParse::FillPackageInfo(manifest_x* manifest) {
     return false;
   }
 
+  manifest->ns = strdup(kTizenPackageXmlNamespace);
   manifest->package = strdup(app_info->package().c_str());
   manifest->type = strdup("tpk");
+  manifest->appsetting = strdup("false");
+  manifest->nodisplay_setting = strdup("false");
+  manifest->support_disable = strdup("false");
   manifest->version = strdup(app_info->version().c_str());
   manifest->installlocation = strdup(app_info->install_location().c_str());
   manifest->api_version = strdup(app_info->api_version().c_str());
@@ -139,6 +154,7 @@ bool StepParse::FillAuthorInfo(manifest_x* manifest) {
   author->text = strdup(author_info->name().c_str());
   author->email = strdup(author_info->email().c_str());
   author->href = strdup(author_info->href().c_str());
+  author->lang = strdup(DEFAULT_LOCALE);
   manifest->author = g_list_append(manifest->author, author);
   return true;
 }
@@ -155,8 +171,9 @@ bool StepParse::FillDescription(manifest_x* manifest) {
 
   description_x* description = reinterpret_cast<description_x*>
       (calloc(1, sizeof(description_x)));
-  description->name = strdup(description_info->description().c_str());
-  description->lang = strdup(description_info->xml_lang().c_str());
+  description->text = strdup(description_info->description().c_str());
+  description->lang = !description_info->xml_lang().empty() ?
+      strdup(description_info->xml_lang().c_str()) : strdup(DEFAULT_LOCALE);
   manifest->description = g_list_append(manifest->description, description);
   return true;
 }
@@ -184,15 +201,36 @@ bool StepParse::FillServiceApplication(manifest_x* manifest) {
     return true;
 
   for (const auto& application : service_application_list->items) {
+    // if there is no app yet, set this app as mainapp
+    bool main_app = manifest->application == nullptr;
+
     application_x* service_app =
         static_cast<application_x*>(calloc(1, sizeof(application_x)));
     service_app->appid = strdup(application.sa_info.appid().c_str());
     service_app->autorestart =
         strdup(application.sa_info.auto_restart().c_str());
-    service_app->exec = strdup(application.sa_info.exec().c_str());
+    service_app->exec = strdup((context_->root_application_path.get()
+                           / manifest->package / "bin"
+                           / application.sa_info.exec()).c_str());
     service_app->onboot = strdup(application.sa_info.on_boot().c_str());
     service_app->type = strdup(application.sa_info.type().c_str());
+    service_app->ui_gadget = strdup("false");
+    service_app->nodisplay = strdup("false");
+    service_app->taskmanage = strdup("true");
+    service_app->multiple = strdup("false");
     service_app->component_type = strdup("svcapp");
+    service_app->mainapp = main_app ? strdup("true") : strdup("false");
+    service_app->enabled = strdup("true");
+    service_app->hwacceleration = strdup("default");
+    service_app->screenreader = strdup("use-system-setting");
+    service_app->recentimage = strdup("false");
+    service_app->launchcondition = strdup("false");
+    service_app->indicatordisplay = strdup("true");
+    service_app->guestmode_visibility = strdup("true");
+    service_app->permission_type = strdup("normal");
+    service_app->submode = strdup("false");
+    service_app->process_pool = strdup("false");
+    service_app->package = strdup(manifest->package);
     manifest->application = g_list_append(manifest->application, service_app);
 
     if (!FillAppControl(service_app,  application.app_control))
@@ -217,16 +255,36 @@ bool StepParse::FillUIApplication(manifest_x* manifest) {
     return true;
 
   for (const auto& application : ui_application_list->items) {
+    // if there is no app yet, set this app as mainapp
+    bool main_app = manifest->application == nullptr;
+
     application_x* ui_app =
         static_cast<application_x*>(calloc(1, sizeof(application_x)));
     ui_app->appid = strdup(application.ui_info.appid().c_str());
-    ui_app->exec = strdup(application.ui_info.exec().c_str());
+    ui_app->exec = strdup((context_->root_application_path.get()
+                           / manifest->package / "bin"
+                           / application.ui_info.exec()).c_str());
     ui_app->launch_mode = strdup(application.ui_info.launch_mode().c_str());
     ui_app->multiple = strdup(application.ui_info.multiple().c_str());
     ui_app->nodisplay = strdup(application.ui_info.nodisplay().c_str());
     ui_app->taskmanage = strdup(application.ui_info.taskmanage().c_str());
     ui_app->type = strdup(application.ui_info.type().c_str());
+    ui_app->ui_gadget = strdup("false");
+    ui_app->onboot = strdup("false");
+    ui_app->autorestart = strdup("false");
     ui_app->component_type = strdup("uiapp");
+    ui_app->mainapp = main_app ? strdup("true") : strdup("false");
+    ui_app->enabled = strdup("true");
+    ui_app->hwacceleration = strdup("default");
+    ui_app->screenreader = strdup("use-system-setting");
+    ui_app->recentimage = strdup("false");
+    ui_app->launchcondition = strdup("false");
+    ui_app->indicatordisplay = strdup("true");
+    ui_app->guestmode_visibility = strdup("true");
+    ui_app->permission_type = strdup("normal");
+    ui_app->submode = strdup("false");
+    ui_app->process_pool = strdup("false");
+    ui_app->package = strdup(manifest->package);
     manifest->application = g_list_append(manifest->application, ui_app);
 
     if (!FillAppControl(ui_app, application.app_control))
@@ -288,6 +346,7 @@ bool StepParse::FillApplicationIconPaths(application_x* app,
     // Current implementation is just for compatibility.
     icon->text = strdup(application_icon.path().c_str());
     icon->name = strdup(application_icon.path().c_str());
+    icon->lang = strdup(DEFAULT_LOCALE);
     app->icon = g_list_append(app->icon, icon);
   }
   return true;
@@ -306,7 +365,8 @@ bool StepParse::FillLabel(application_x* app, const T& label_list) {
     // Current implementation is just for compatibility.
     label->text = strdup(control.text().c_str());
     label->name = strdup(control.name().c_str());
-    label->lang = strdup(control.xml_lang().c_str());
+    label->lang = !control.xml_lang().empty() ?
+        strdup(control.xml_lang().c_str()) : strdup(DEFAULT_LOCALE);
     app->label = g_list_append(app->label, label);
   }
   return true;
@@ -373,6 +433,8 @@ bool StepParse::FillShortcuts() {
 
 bool StepParse::FillManifestX(manifest_x* manifest) {
   if (!FillPackageInfo(manifest))
+    return false;
+  if (!FillInstallationInfo(manifest))
     return false;
   if (!FillUIApplication(manifest))
     return false;
