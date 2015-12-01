d02693d1e7576631db8847d3d7b78efdc3e726e1
Lukasz Wysocki <l.wysocki@samsung.com>
2015-12-01 16:07:18 +0100

    Add handling for background-category element in TPK.
    Refactoring - remove ISpecification from the solution.
    
    This commit depend on:
    - https://review.tizen.org/gerrit/#/c/53072/
    - https://review.tizen.org/gerrit/#/c/50502/
    
    changes should be submitted together.
    
    Fix code review from:
    - https://review.tizen.org/gerrit/#/c/50502/
    
    Fix issue from:
    - https://review.tizen.org/gerrit/#/c/53835/
    
    Change-Id: I8ca81da641e724822c96b6606343a619673b63a2

---
+++ src/common/step/step_check_background_category.h
@@ -0,0 +1,101 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_STEP_STEP_CHECK_BACKGROUND_CATEGORY_H_
+#define COMMON_STEP_STEP_CHECK_BACKGROUND_CATEGORY_H_
+
+#include <manifest_parser/utils/version_number.h>
+
+#include <glib.h>
+#include <set>
+#include <string>
+
+#include "common/step/step.h"
+#include "common/utils/logging.h"
+
+namespace common_installer {
+namespace security {
+
+typedef std::set<std::string> BackgroundCatSet;
+
+/**
+ * \brief This step check background category value and modify it depending on
+ *        required version, cert level, background support, and value itself
+ */
+class StepCheckBackgroundCategory : public common_installer::Step {
+ public:
+  using Step::Step;
+
+  explicit StepCheckBackgroundCategory(
+      common_installer::InstallerContext* context);
+
+  /**
+   * \brief Check background category values
+   *
+   * \return Status::ERROR when "system" detected,
+   *         Status::OK otherwise
+   */
+  Status process() override;
+
+  Status clean() override { return Status::OK; }
+  Status undo() override { return Status::OK; }
+  Status precheck() override { return Status::OK; }
+
+ protected:
+  virtual bool GetBackgroundSupport() = 0;
+
+ private:
+  GList* CopyValuesToBackgroundCategory(
+      const BackgroundCatSet& values, GList* backgroundCategories);
+  bool IsTrustedCert(common_installer::PrivilegeLevel privilege) const;
+  bool ShouldSendFail(common_installer::PrivilegeLevel privilege,
+                      const BackgroundCatSet& background_categories) const;
+  bool ShouldSendAll(const utils::VersionNumber& version,
+                     bool background_support,
+                     common_installer::PrivilegeLevel privilege,
+                     const BackgroundCatSet& background_categories) const;
+  bool ShouldSendSystem(common_installer::PrivilegeLevel privilege,
+                        const BackgroundCatSet& background_categories) const;
+  bool ShouldSendKnown(const utils::VersionNumber& version,
+                       bool background_support,
+                       common_installer::PrivilegeLevel privilege,
+                       const BackgroundCatSet& background_categories) const;
+  bool ShouldSendUnknown(common_installer::PrivilegeLevel privilege,
+                         const BackgroundCatSet& background_categories) const;
+
+  common_installer::Step::Status DoSendFail(
+      common_installer::PrivilegeLevel privilege,
+      const BackgroundCatSet& background_categories);
+  void DoSendAll(const utils::VersionNumber& version,
+                 bool background_support,
+                 common_installer::PrivilegeLevel privilege,
+                 const BackgroundCatSet& background_categories,
+                 application_x* app);
+  void DoSendSystem(common_installer::PrivilegeLevel privilege,
+                    const BackgroundCatSet& background_categories,
+                    application_x* app);
+  void DoSendKnown(const utils::VersionNumber& version,
+                   bool background_support,
+                   common_installer::PrivilegeLevel privilege,
+                   const BackgroundCatSet& background_categories,
+                   application_x* app);
+  void DoSendUnknown(common_installer::PrivilegeLevel privilege,
+                     const BackgroundCatSet& background_categories,
+                     application_x* app);
+
+  void GetBackgroundCategories(
+      const application_x& app,
+      BackgroundCatSet* background_categories);
+  void RemoveContextBackgroundCategories(application_x* app);
+
+  const BackgroundCatSet known_background_categories_;
+  const BackgroundCatSet not_unknown_background_categories_;
+
+  SCOPE_LOG_TAG(CheckBackgroundCategory)
+};
+
+}  // namespace security
+}  // namespace common_installer
+
+#endif  // COMMON_STEP_STEP_CHECK_BACKGROUND_CATEGORY_H_
