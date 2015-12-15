630f86f968b4597d3fb63e189f85a77f9ed6fc4b
Sangyoon Jang <s89.jang@samsung.com>
2015-12-15 13:56:22 +0900

    Parse and fill widget-application tag
    Change-Id: I7cb53a1834e87f70c8af47e8895ecbd0c756a486
    Signed-off-by: Sangyoon Jang <s89.jang@samsung.com>

--- src/common/step/step_generate_xml.h
+++ src/common/step/step_generate_xml.h
@@ -26,9 +26,15 @@ class StepGenerateXml : public common_installer::Step {
   Status precheck() override;
 
  private:
+  enum class AppCompType {
+    UIAPP,
+    SVCAPP,
+    WIDGETAPP
+  };
+
   Step::Status GenerateApplicationCommonXml(application_x* app,
                                             xmlTextWriterPtr writer,
-                                            bool is_service);
+                                            AppCompType type);
 
   SCOPE_LOG_TAG(GenerateXML)
 };
