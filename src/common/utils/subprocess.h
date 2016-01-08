84924c74a90d126e745e6ca9177c2e09471a24fc
Tomasz Iwanek <t.iwanek@samsung.com>
2016-01-08 13:58:32 +0100

    Subprocess utility
    Change-Id: I6efadead6c4eed68e38746b3dcbb615fbde5a921

---
+++ src/common/utils/subprocess.h
@@ -0,0 +1,34 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache-2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_UTILS_SUBPROCESS_H_
+#define COMMON_UTILS_SUBPROCESS_H_
+
+#include <string>
+#include <vector>
+
+namespace common_installer {
+
+class Subprocess {
+ public:
+  explicit Subprocess(const std::string& program);
+
+  template<typename ...Args> bool Run(Args&&... args) {
+    std::vector<std::string> argv{std::forward<Args>(args)...};
+    return RunWithArgs(argv);
+  }
+
+  bool RunWithArgs(
+      const std::vector<std::string>& args = std::vector<std::string>());
+  int Wait();
+
+ private:
+  std::string program_;
+  int pid_;
+  bool started_;
+};
+
+}  // namespace common_installer
+
+#endif  // COMMON_UTILS_SUBPROCESS_H_
