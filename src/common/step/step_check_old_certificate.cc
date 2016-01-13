fa949a727ecd50465dc7ddb65fd6282cb9ce12d0
Arkadiusz Szulakiewicz <a.szulakiewi@partner.samsung.com>
2016-01-13 16:04:55 +0100

    Fail installation if certificate comparision fails
    Change-Id: Ie07c7361d057b0d4eabc7e576719fce1152994c3

--- src/common/step/step_check_old_certificate.cc
+++ src/common/step/step_check_old_certificate.cc
@@ -4,6 +4,11 @@
 
 #include "common/step/step_check_old_certificate.h"
 
+#include <gum/gum-user.h>
+#include <gum/common/gum-user-types.h>
+#include <gum/gum-user-service.h>
+#include <gum/common/gum-string-utils.h>
+
 #include <manifest_parser/utils/logging.h>
 #include <pkgmgr-info.h>
 #include <unistd.h>
@@ -12,15 +17,69 @@
 #include <string>
 
 #include "common/pkgmgr_registration.h"
+#include "common/utils/glist_range.h"
 
 namespace common_installer {
 namespace security {
+
+bool CheckPkgCertificateMismatch(const std::string& pkgid,
+                                 const uid_t current_uid,
+                                 const std::string& old_certificate) {
+  gchar **users_types = NULL;
+  users_types = gum_string_utils_append_string(nullptr, "normal");
+  users_types = gum_string_utils_append_string(users_types, "admin");
+  users_types = gum_string_utils_append_string(users_types, "guest");
+
+  GumUserService *user_service = NULL;
+  user_service = gum_user_service_create_sync(FALSE);
+
+  GumUserList *users = NULL;
+  users = gum_user_service_get_user_list_sync(user_service,
+                                             (const gchar *const *)users_types);
+  g_strfreev(users_types);
+
+  bool certificate_mismatch = false;
+
+  if (users) {
+    uid_t uid = G_MAXUINT;
+
+    for (GumUser* guser : GListRange<GumUser*>(users)) {
+      g_object_get(G_OBJECT(guser), "uid", &uid, NULL);
+      if (current_uid == uid) continue;
+
+      auto certificate = QueryCertificateAuthorCertificate(pkgid, uid);
+      if (!certificate.empty()) {
+        certificate_mismatch = (old_certificate != certificate);
+        if (certificate_mismatch)
+          LOG(DEBUG) << "Found app with mismatched certificate: " << pkgid;
+      }
+    }
+    gum_user_service_list_free(users);
+  }
+
+  g_object_unref(user_service);
+  return certificate_mismatch;
+}
+
 
 Step::Status StepCheckOldCertificate::process() {
   std::string old_author_certificate =
       QueryCertificateAuthorCertificate(context_->pkgid.get(),
                                         context_->uid.get());
   const auto& cert = context_->certificate_info.get().author_certificate.get();
+
+  if (cert) {
+    bool certificate_mismatch =
+        CheckPkgCertificateMismatch(context_->pkgid.get(),
+                                    context_->uid.get(),
+                                    cert->getBase64());
+    if (certificate_mismatch) {
+      LOG(ERROR) << "Package with the same id and different certificate "
+                 << "has been already installed";
+      return Status::CERT_ERROR;
+    }
+  }
+
   if (!old_author_certificate.empty()) {
     if (!cert) {
       LOG(ERROR) << "Trying to update package without signature is not allowed "
