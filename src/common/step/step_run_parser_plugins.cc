74f6a82252f6f12096d123e4d10329126c1a82bb
Lukasz Wartalowicz <l.wartalowic@samsung.com>
2016-01-18 15:56:59 +0100

    Add step StepRunParserPlugin
    Change-Id: Ib644cf44a47879750c99026974f8c83909eb8a66

---
+++ src/common/step/step_run_parser_plugins.cc
@@ -0,0 +1,82 @@
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
+
+#include <string>
+#include <vector>
+
+#include "common/step/step_run_parser_plugins.h"
+
+namespace common_installer {
+namespace pkgmgr {
+
+StepRunParserPlugin::StepRunParserPlugin(InstallerContext* context,
+                                         ActionType action_type)
+    : Step(context), action_type_(action_type) {}
+
+Step::Status StepRunParserPlugin::process() {
+  const boost::filesystem::path xmlPath(context_->xml_path.get());
+
+  pkgid_ = context_->pkgid.get();
+
+  // PLUGINS_LIST_FILE_PATH path generated from cmake
+  const std::string listPath(PLUGINS_LIST_INSTALL_FILE_PATH);
+
+  // init plugin_manager
+  plugin_manager_ = std::unique_ptr<PluginManager>(
+      new PluginManager(xmlPath.string(), listPath));
+
+  if (plugin_manager_->GenerateUnknownTagList()) {
+    std::vector<std::shared_ptr<PluginInfo>> plugins =
+        plugin_manager_->UnknownTagList();
+
+    for (const std::shared_ptr<PluginInfo>& plugin : plugins) {
+      if (!plugin_manager_->Launch(plugin->path(), action_type_, pkgid_)) {
+        LOG(ERROR) << "Error during launch tag name: " << plugin->name()
+                   << " path: " << plugin->path();
+        return Status::ERROR;
+      }
+      // add plugin to array for undo process
+      if (action_type_ == ActionType::Install) {
+        installed_plugins_.push_back(plugin);
+      }
+    }
+  } else {
+    LOG(ERROR) << "Error during generate unknown list, no exist file, path, "
+                  "invalid data format etc, chceck log";
+    return Status::ERROR;
+  }
+
+  LOG(INFO) << "Successfully processing plugins";
+  return Status::OK;
+}
+
+Step::Status StepRunParserPlugin::clean() {
+  // nothing to do
+  return Status::OK;
+}
+
+Step::Status StepRunParserPlugin::undo() {
+  if (installed_plugins_.empty()) {
+    // no installed plugins
+    return Status::OK;
+  }
+
+  for (const std::shared_ptr<PluginInfo>& plugin : installed_plugins_) {
+    if (!plugin_manager_->Launch(plugin->path(), ActionType::Uninstall,
+                                 pkgid_)) {
+      LOG(ERROR) << "Error during uninstall tag name: " << plugin->name()
+                 << " path: " << plugin->path();
+    }
+  }
+
+  return Status::OK;
+}
+
+Step::Status StepRunParserPlugin::precheck() {
+  // nothing to do
+  return Status::OK;
+}
+
+}  // namespace pkgmgr
+}  // namespace common_installer
---
+++ src/common/utils/plugin_xml_parser.h
@@ -0,0 +1,51 @@
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_UTILS_PLUGIN_XML_PARSER_H_
+#define COMMON_UTILS_PLUGIN_XML_PARSER_H_
+
+#include <libxml2/libxml/xmlreader.h>
+
+#include <string>
+#include <vector>
+#include <set>
+
+namespace common_installer {
+
+/** this class parse xml file*/
+class PluginsXmlParser {
+ public:
+  explicit PluginsXmlParser(const std::string& path)
+      : path_(path), doc_ptr_(nullptr) {}
+  ~PluginsXmlParser();
+  bool Parse();
+  const std::vector<std::string>& tags_list();
+  xmlDocPtr doc_ptr();
+
+ private:
+  const std::string path_;
+  xmlDocPtr doc_ptr_;
+  std::vector<std::string> tags_;
+  int NextChildElement(xmlTextReaderPtr reader, int depth);
+
+  class WrapperXMLReader {
+   public:
+    WrapperXMLReader() : reader_(NULL) {}
+
+    xmlTextReaderPtr Create(const std::string& path) {
+      reader_ = xmlReaderForFile(path.c_str(), NULL, 0);
+      return reader_;
+    }
+    virtual ~WrapperXMLReader() {
+      if (reader_) {
+        xmlFreeTextReader(reader_);
+      }
+    }
+
+   private:
+    xmlTextReaderPtr reader_;
+  };
+};
+}  // namespace common_installer
+#endif  // COMMON_UTILS_PLUGIN_XML_PARSER_H_
