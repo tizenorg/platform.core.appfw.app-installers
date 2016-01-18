49ae9fd6f79bc593c84cf88e9cce03de84937ea5
Kamil Chlodnicki <k.chlodnicki@samsung.com>
2016-01-18 11:19:07 +0100

    Add PluginsLauncher and DynamicLibHandle
    Change-Id: I55daa5f8d8867e4716530db5d6c9319526bf02fe

---
+++ src/common/utils/dynamic_lib_handle.h
@@ -0,0 +1,52 @@
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_UTILS_DYNAMIC_LIB_HANDLE_H_
+#define COMMON_UTILS_DYNAMIC_LIB_HANDLE_H_
+
+#include <dlfcn.h>
+#include <manifest_parser/utils/logging.h>
+
+#include <memory>
+#include <string>
+
+namespace common_installer {
+
+class DynamicLibHandle {
+ public:
+  static std::unique_ptr<DynamicLibHandle> Create(const std::string& name,
+                                                  int flags);
+
+  template <typename Ret, typename... Args>
+  bool run(const std::string& name, Ret* result, Args&&... args) {
+    using PluginFunctionPtr = Ret (*)(Args...);
+    auto function = reinterpret_cast<PluginFunctionPtr>(GetSymbol(name));
+
+    if (!function) {
+      LOG(ERROR) << "Failed to get symbol: " << name <<" (" << dlerror() << ")";
+      return false;
+    }
+
+    *result = function(std::forward<Args>(args)...);
+    return true;
+  }
+
+  DynamicLibHandle(DynamicLibHandle&&) = default;
+  DynamicLibHandle& operator=(DynamicLibHandle&&) = default;
+
+  ~DynamicLibHandle();
+
+  SCOPE_LOG_TAG(DynamicLibHandle)
+
+ private:
+  DynamicLibHandle();
+  bool CreateImpl(const std::string& name, int flags);
+
+  void* GetSymbol(const std::string& name);
+  void* lib_handle_;
+};
+
+}  // namespace common_installer
+
+#endif  // COMMON_UTILS_DYNAMIC_LIB_HANDLE_H_
