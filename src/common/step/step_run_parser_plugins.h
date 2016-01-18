e99989fde273e789695a9aa2950641dc415a5c89
Lukasz Wartalowicz <l.wartalowic@samsung.com>
2016-01-18 15:56:59 +0100

    Add step StepRunParserPlugin
    Change-Id: Ib644cf44a47879750c99026974f8c83909eb8a66

---
+++ src/common/step/step_run_parser_plugins.h
@@ -0,0 +1,40 @@
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_STEP_STEP_RUN_PARSER_PLUGINS_H_
+#define COMMON_STEP_STEP_RUN_PARSER_PLUGINS_H_
+
+#include <string>
+#include <vector>
+
+#include "common/utils/plugin_manager.h"
+#include "manifest_parser/utils/logging.h"
+#include "common/step/step.h"
+
+namespace common_installer {
+namespace pkgmgr {
+
+class StepRunParserPlugin : public Step {
+ public:
+  explicit StepRunParserPlugin(InstallerContext* context,
+                               ActionType action_type);
+
+  Step::Status process() override;
+  Step::Status clean() { return Status::OK; }
+  Step::Status undo() override;
+  Step::Status precheck() { return Status::OK; }
+
+  SCOPE_LOG_TAG(RunParserPlugin)
+
+ private:
+  Step::Status ProcessingPlugins(const boost::filesystem::path& xml_path);
+  ActionType action_type_;
+  std::vector<std::shared_ptr<PluginInfo>> installed_plugins_;
+  std::shared_ptr<PluginManager> plugin_manager_;
+};
+
+}  // namespace pkgmgr
+}  // namespace common_installer
+
+#endif  //  COMMON_STEP_STEP_RUN_PARSER_PLUGINS_H_
