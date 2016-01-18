1a2b4acaf40477ab0823ce7d8a6fb5b86004cf0b
Kamil Chlodnicki <k.chlodnicki@samsung.com>
2016-01-18 11:19:07 +0100

    Add PluginsLauncher and DynamicLibHandle
    Change-Id: I55daa5f8d8867e4716530db5d6c9319526bf02fe

---
+++ src/common/utils/dynamic_lib_handle.cc
@@ -0,0 +1,38 @@
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
+
+#include "common/utils/dynamic_lib_handle.h"
+
+#include <memory>
+
+namespace common_installer {
+
+DynamicLibHandle::DynamicLibHandle() : lib_handle_(nullptr) {}
+
+std::unique_ptr<DynamicLibHandle> DynamicLibHandle::Create(
+    const std::string& name, int flags) {
+  std::unique_ptr<DynamicLibHandle> new_handle(new DynamicLibHandle);
+  if (!new_handle->CreateImpl(name, flags)) {
+    return nullptr;
+  }
+  return new_handle;
+}
+
+bool DynamicLibHandle::CreateImpl(const std::string& name, int flags) {
+  lib_handle_ = dlopen(name.c_str(), flags);
+  if (!lib_handle_) {
+    LOG(ERROR) << "Failed to open library: " << name << " (" << dlerror()
+               << ")";
+    return false;
+  }
+  return true;
+}
+
+void* DynamicLibHandle::GetSymbol(const std::string& name) {
+  return dlsym(lib_handle_, name.c_str());
+}
+
+DynamicLibHandle::~DynamicLibHandle() { dlclose(lib_handle_); }
+
+}  // namespace common_installer
