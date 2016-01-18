5d3294093818ec79138b6df2be974c4829f01d71
Lukasz Wartalowicz <l.wartalowic@samsung.com>
2016-01-18 15:56:59 +0100

    Add step StepRunParserPlugin
    Change-Id: Ib644cf44a47879750c99026974f8c83909eb8a66

---
+++ src/common/utils/plugin_xml_parser.cc
@@ -0,0 +1,86 @@
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
+
+#include "manifest_parser/utils/logging.h"
+#include "common/utils/plugin_xml_parser.h"
+
+namespace common_installer {
+
+bool PluginsXmlParser::Parse() {
+  if (path_.empty()) {
+    return false;
+  }
+
+  WrapperXMLReader objReader;
+
+  xmlTextReaderPtr reader = objReader.Create(path_.c_str());
+
+  if (!reader) {
+    return false;
+  }
+
+  LOG(INFO) << "Xml path: " << path_;
+
+  tags_.clear();
+
+  int depth = xmlTextReaderDepth(reader);
+
+  // use set to remove duplicate
+  std::set<std::string> tags;
+
+  while ((NextChildElement(reader, depth))) {
+    const xmlChar* node = xmlTextReaderConstName(reader);
+
+    if (!node) {
+      return false;
+    }
+
+    tags.insert(std::string((const char*)node));
+  }
+
+  tags_.assign(tags.begin(), tags.end());
+
+  // get here, after end of a "reading" file
+  docPtr_ = xmlTextReaderCurrentDoc(reader);
+
+  if (!docPtr_) {
+    return false;
+  }
+
+  return true;
+}
+
+std::vector<std::string>& PluginsXmlParser::TagsList() { return tags_; }
+
+xmlDocPtr PluginsXmlParser::DocPtr() { return docPtr_; }
+
+int PluginsXmlParser::NextChildElement(xmlTextReaderPtr reader, int depth) {
+  int ret = xmlTextReaderRead(reader);
+  int cur = xmlTextReaderDepth(reader);
+
+  while (ret == 1) {
+    switch (xmlTextReaderNodeType(reader)) {
+      case XML_READER_TYPE_ELEMENT:
+        if (cur == depth + 1) return 1;
+        break;
+      default:
+
+        if (cur <= depth) return 0;
+        break;
+    }
+
+    ret = xmlTextReaderRead(reader);
+    cur = xmlTextReaderDepth(reader);
+  }
+
+  return ret;
+}
+
+PluginsXmlParser::~PluginsXmlParser() {
+  if (docPtr_) {
+    xmlFreeDoc(docPtr_);
+  }
+}
+
+}  // namespace common_installer
