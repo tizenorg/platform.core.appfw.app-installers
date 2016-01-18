b89c84b299eba04a2748d41a4b0fd854f487d3d7
Lukasz Wartalowicz <l.wartalowic@samsung.com>
2016-01-18 15:56:59 +0100

    Add step StepRunParserPlugin
    Change-Id: Ib644cf44a47879750c99026974f8c83909eb8a66

---
+++ src/common/utils/plugin_manager.h
@@ -0,0 +1,38 @@
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_UTILS_PLUGIN_MANAGER_H_
+#define COMMON_UTILS_PLUGIN_MANAGER_H_
+
+#include <boost/filesystem/path.hpp>
+
+#include <string>
+#include <vector>
+
+#include "common/utils/plugin_xml_parser.h"
+#include "common/utils/plugin_list_parser.h"
+
+namespace common_installer {
+
+enum class ActionType { Install, Upgrade, Uninstall };
+
+/** this class manages XML and plugin lists */
+class PluginManager {
+ public:
+  PluginManager(const std::string& xml_path, const std::string& list_path)
+      : xml_parser_(xml_path), list_parser_(list_path) {}
+
+  bool GenerateUnknownTagList();
+  const std::vector<std::shared_ptr<PluginInfo>>& UnknownTagList();
+  bool Launch(const boost::filesystem::path& pluginPath, ActionType actionType,
+              const std::string& pkgId);
+
+ private:
+  std::vector<std::shared_ptr<PluginInfo>> tags_;
+  PluginsXmlParser xml_parser_;
+  PluginsListParser list_parser_;
+};
+}  // namespace common_installer
+
+#endif  // COMMON_UTILS_PLUGIN_MANAGER_H_
