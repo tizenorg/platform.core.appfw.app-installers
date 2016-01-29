1e627511318111613af12c28e2e02a577d3bb0b5
Lukasz Wartalowicz <l.wartalowic@samsung.com>
2016-01-29 15:17:59 +0100

    Integrating StepRunParserPlugin with PluginsLauncher/DynamicLibHandle
    Change-Id: Iaaf1c62250d01573d1960cb1ff5ecdb9cfcf3850

--- src/common/utils/dynamic_lib_handle.h
+++ src/common/utils/dynamic_lib_handle.h
@@ -17,18 +17,24 @@ namespace common_installer {
 
 class DynamicLibHandle {
  public:
-  static std::unique_ptr<DynamicLibHandle> Create(
-      const boost::filesystem::path& path, int flags);
+  DynamicLibHandle();
+  bool Create(const boost::filesystem::path& path, int flags);
 
   template <typename Ret, typename... Args>
-  bool run(const std::string& name, Ret* result, Args&&... args) {
-    using PluginFunctionPtr = Ret (*)(Args...);
-    auto function = reinterpret_cast<PluginFunctionPtr>(GetSymbol(name));
+  bool Exec(const std::string& name, Ret* result, Args&&... args) {
+    using PluginFunctionPtr = Ret (*)(Args&&...);
+    PluginFunctionPtr function =
+        reinterpret_cast<PluginFunctionPtr>(GetSymbol(name));
 
     if (!function) {
-      LOG(ERROR) << "Failed to get symbol: " << name <<" (" << dlerror() << ")";
+      LOG(ERROR) << "Failed to get symbol: " << name << " (" << dlerror()
+                 << ")";
       return false;
     }
+
+    LOG(DEBUG) << "Function parameters: ";
+    using expander = int[];
+    (void)expander{0, (void(LOG(DEBUG) << ' ' << (args)), 0)...};
 
     *result = function(std::forward<Args>(args)...);
     return true;
@@ -37,14 +43,11 @@ class DynamicLibHandle {
   DynamicLibHandle(DynamicLibHandle&&) = default;
   DynamicLibHandle& operator=(DynamicLibHandle&&) = default;
 
-  ~DynamicLibHandle();
+  virtual ~DynamicLibHandle();
 
   SCOPE_LOG_TAG(DynamicLibHandle)
 
  private:
-  DynamicLibHandle();
-  bool CreateImpl(const boost::filesystem::path& path, int flags);
-
   void* GetSymbol(const std::string& name);
   void* lib_handle_;
 };
