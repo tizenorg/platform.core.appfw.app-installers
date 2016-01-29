91bc12234251ff7e57d69f36d7de2b492f08cf4a
Lukasz Wartalowicz <l.wartalowic@samsung.com>
2016-01-29 15:17:59 +0100

    Integrating StepRunParserPlugin with PluginsLauncher/DynamicLibHandle
    Change-Id: Iaaf1c62250d01573d1960cb1ff5ecdb9cfcf3850

--- src/common/utils/plugin_manager.cc
+++ src/common/utils/plugin_manager.cc
@@ -66,13 +66,31 @@ PluginManager::UnknownTagList() {
 }
 
 bool PluginManager::Launch(const boost::filesystem::path& plugin_path,
-                           ActionType action_type, const std::string& pkg_Id) {
-  // TODO(l.wartalowic) add implementation
+                           PluginsLauncher::ActionType action_type,
+                           const std::string& pkg_Id) {
   LOG(INFO) << "Launching plugin path:" << plugin_path << " pkgId: " << pkg_Id;
-  (void)plugin_path;
-  (void)action_type;
-  (void)pkg_Id;
-  return true;
+
+  int result = EPERM;
+
+  PluginsLauncher::Error error = plugins_launcher_.LaunchPlugin(
+      plugin_path, xml_parser_.doc_ptr(), action_type, pkg_Id, &result);
+
+  switch (error) {
+    case PluginsLauncher::Error::Success: {
+      if (result != 0) {
+        LOG(ERROR) << "Error from plugin lib: " << plugin_path
+                   << " error code: " << result;
+        return false;
+      }
+      return true;
+    }
+    case PluginsLauncher::Error::ActionNotSupported:
+      return true;
+
+    case PluginsLauncher::Error::FailedLibHandle:
+    default:
+      return false;
+  }
 }
 
 }  // namespace common_installer
