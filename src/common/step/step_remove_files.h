386d7194a39de111cde1267bd675e604a68b2dac
Arkadiusz Szulakiewicz <a.szulakiewi@partner.samsung.com>
2016-02-04 17:34:27 +0100

    Minor improvements for force-uninstall step
    Change-Id: If2fe882812653272f2b5724e5a37e2f01a914214

--- src/common/step/step_remove_files.h
+++ src/common/step/step_remove_files.h
@@ -19,8 +19,8 @@ class StepRemoveFiles : public Step {
   using Step::Step;
 
   Status process() override;
-  Status clean() override;
-  Status undo() override;
+  Status clean() override { return Status::OK; }
+  Status undo() override  { return Status::OK; }
   Status precheck() override;
 
   SCOPE_LOG_TAG(Remove)
