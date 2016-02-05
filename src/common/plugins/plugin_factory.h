7e3d966fd81d10db0458b35c46b7a4509c7bb77b
Tomasz Iwanek <t.iwanek@samsung.com>
2016-02-05 13:39:21 +0100

    Implement launching 'metadata' and 'category' plugin
    Implementation of loading other types of plugins of
    pkgmgr then 'tag':
     - DynamicLibHandle was reworked to be based class of all plugins types,
     - add factory for plugin and common interface for them.
    
    Following changes should be submitted together:
     - https://review.tizen.org/gerrit/#/c/59029/
     - https://review.tizen.org/gerrit/#/c/59028/
     - https://review.tizen.org/gerrit/#/c/59030/
     - https://review.tizen.org/gerrit/#/c/59031/
    
    Change-Id: I7f6bd50e364bedac2ee18626db0b02cda4865e31

---
+++ src/common/plugins/plugin_factory.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache-2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_PLUGINS_PLUGIN_FACTORY_H_
+#define COMMON_PLUGINS_PLUGIN_FACTORY_H_
+
+#include <memory>
+
+#include "common/plugins/plugin.h"
+#include "common/plugins/plugin_list_parser.h"
+
+namespace common_installer {
+
+class PluginFactory {
+ public:
+  PluginFactory() { }
+  std::unique_ptr<Plugin> CreatePluginByPluginInfo(
+      const PluginInfo& plugin_info);
+};
+
+}  // namespace common_installer
+
+#endif  // COMMON_PLUGINS_PLUGIN_FACTORY_H_
