d1e7169840c3abfb940e777607948ab8a8f28416
Arkadiusz Szulakiewicz <a.szulakiewi@partner.samsung.com>
2016-02-15 14:16:53 +0100

    Implement creating shared directories
    This patch has dependency on the following change:
    https://review.tizen.org/gerrit/#/c/59696/
    
    Change-Id: If34c3c5e91ca3c485bb8c99c169e59c9fdd2959d

---
+++ src/common/step/step_remove_shared_directories.cc
@@ -0,0 +1,34 @@
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
+
+#include "common/step/step_remove_shared_directories.h"
+
+#include <string>
+#include <vector>
+
+#include "common/installer_context.h"
+#include "common/pkgmgr_registration.h"
+#include "common/utils/subprocess.h"
+
+namespace common_installer {
+namespace filesystem {
+
+Step::Status StepRemoveSharedDirectories::process() {
+  if (GLOBAL_USER != getuid()) return Step::Status::OK;
+
+  std::string package_id = context_->pkgid.get();
+
+  Subprocess pkgdir_tool_process("/usr/bin/pkgdir-tool");
+  pkgdir_tool_process.RunWithArgs({"--delete", "--pkgid", package_id});
+  int result = pkgdir_tool_process.Wait();
+  if (result) {
+    LOG(ERROR) << "Failed to delete shared dirs for users";
+    return Step::Status::APP_DIR_ERROR;
+  }
+  return Step::Status::OK;
+}
+
+}  // namespace filesystem
+}  // namespace common_installer
+
