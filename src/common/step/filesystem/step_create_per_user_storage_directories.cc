4e7a11669752319045203e61635ab3a37b694ad0
Arkadiusz Szulakiewicz <a.szulakiewi@partner.samsung.com>
2016-03-08 15:12:49 +0100

    Create shared directories on external storage
    Change-Id: I6cbccde9b6f18848c4a6f9bb09bbfbcfb8078412

--- src/common/step/filesystem/step_create_per_user_storage_directories.cc
+++ src/common/step/filesystem/step_create_per_user_storage_directories.cc
@@ -3,15 +3,21 @@
 // found in the LICENSE file.
 
 #include <string>
+#include <vector>
 
 #include "common/utils/subprocess.h"
 #include "common/step/filesystem/step_create_per_user_storage_directories.h"
+
+#include "common/shared_dirs.h"
+#include "common/utils/glist_range.h"
 
 namespace common_installer {
 namespace filesystem {
 
 common_installer::Step::Status StepCreatePerUserStorageDirectories::process() {
+  if (!CreateExternalStorageDir()) return Step::Status::APP_DIR_ERROR;
   if (GLOBAL_USER != context_->uid.get()) return Step::Status::OK;
+
   std::string package_id = context_->pkgid.get();
   LOG(INFO) << "Creating per-user directories for package: " << package_id;
 
@@ -23,6 +29,57 @@ common_installer::Step::Status StepCreatePerUserStorageDirectories::process() {
     return Status::APP_DIR_ERROR;
   }
   return Status::OK;
+}
+
+bool StepCreatePerUserStorageDirectories::CreateExternalStorageDir() {
+  auto manifest = context_->manifest_data.get();
+    bool has_external_storage_priv = false;
+    const char* privilege =
+        "http://tizen.org/privilege/externalstorage.appdata";
+    for (const char* priv : GListRange<char*>(manifest->privileges)) {
+      if (strcmp(priv, privilege) == 0) {
+        has_external_storage_priv = true;
+        LOG(DEBUG) << "External storage privilege has been found.";
+        break;
+      }
+    }
+    if (!has_external_storage_priv) {
+      LOG(DEBUG) << "External storage privilege not found, skipping.";
+      return true;
+    }
+    std::vector<std::string> pkg_ids = { context_->pkgid.get() };
+    auto pkg_list = CreatePkgInformationList(context_->uid.get(), pkg_ids);
+
+    if (pkg_list.empty()) {
+      LOG(ERROR) << "Could not create pkg information list.";
+      return false;
+    }
+
+    PkgInfo pkg = *pkg_list.begin();
+    bool create_skel_dirs = false;
+
+    switch (context_->request_mode.get()) {
+      case RequestMode::GLOBAL: {
+        LOG(DEBUG) << "Creating external directories for all users";
+        PerformExternalDirectoryCreationForAllUsers(pkg.pkg_id,
+                                                         pkg.author_id,
+                                                         pkg.api_version,
+                                                         create_skel_dirs);
+      }
+      break;
+      case RequestMode::USER: {
+        LOG(DEBUG) << "Creating external directories for user: "
+            << context_->uid.get();
+        PerformExternalDirectoryCreationForUser(context_->uid.get(),
+                                                     pkg.pkg_id,
+                                                     pkg.author_id,
+                                                     pkg.api_version,
+                                                     create_skel_dirs);
+      }
+      break;
+    }
+
+    return true;
 }
 
 }  // namespace filesystem
