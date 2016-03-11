69a559dae8c9e16b9b998e516b8ff39cf5a376c9
Sangyoon Jang <s89.jang@samsung.com>
2016-03-11 14:47:05 +0900

    Add StepCheckRemovable
    This step checks the given package is removable or not.
    
    Change-Id: I63ddfcfd8dbd2f100e413c2c5c6b1290842ac590
    Signed-off-by: Sangyoon Jang <s89.jang@samsung.com>

---
+++ src/common/step/step_check_removable.h
@@ -0,0 +1,28 @@
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache 2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_STEP_STEP_CHECK_REMOVABLE_H_
+#define COMMON_STEP_STEP_CHECK_REMOVABLE_H_
+
+#include "common/step/step.h"
+
+namespace common_installer {
+namespace pkgmgr {
+
+class StepCheckRemovable : public Step {
+ public:
+  using Step::Step;
+
+  Status process() override;
+  Status undo() override { return Status::OK; }
+  Status clean() override { return Status::OK; }
+  Status precheck() override;
+
+  SCOPE_LOG_TAG(CheckRemovable)
+};
+
+}  // namespace pkgmgr
+}  // namespace common_installer
+
+#endif  // COMMON_STEP_STEP_CHECK_REMOVABLE_H_
