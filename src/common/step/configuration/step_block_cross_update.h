0d42c821101c9123a3dbf38981a1d715f57ed76b
Tomasz Iwanek <t.iwanek@samsung.com>
2016-04-08 15:48:53 +0200

    Mount install steps
    Requires:
     - https://review.tizen.org/gerrit/#/c/65788/
    
    Change-Id: I37974991f0d5fa2248e0a40aa1a999cc389f7485

---
+++ src/common/step/configuration/step_block_cross_update.h
@@ -0,0 +1,44 @@
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache-2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_STEP_CONFIGURATION_STEP_BLOCK_CROSS_UPDATE_H_
+#define COMMON_STEP_CONFIGURATION_STEP_BLOCK_CROSS_UPDATE_H_
+
+#include <manifest_parser/utils/logging.h>
+
+#include "common/installer_context.h"
+
+#include "common/step/step.h"
+
+namespace common_installer {
+namespace configuration {
+
+/**
+ * @brief The StepBlockCrossUpdate class
+ *        This step is used to block update of package in case when installation
+ *        was performed in different way than update.
+ *
+ * Packages that where installed by mount-install cannot be update with normal
+ * installation and packages that where installed by normal installation cannot
+ * be updated with mount update.
+ *
+ * We cannot use reinstall, RDS and delta update with mount-installed package
+ * in current version.
+ */
+class StepBlockCrossUpdate : public Step {
+ public:
+  using Step::Step;
+
+  Status process() override;
+  Status clean() override { return Status::OK; }
+  Status undo() override { return Status::OK; }
+  Status precheck() override { return Status::OK; }
+
+  SCOPE_LOG_TAG(BlockCrossUpdate)
+};
+
+}  // namespace configuration
+}  // namespace common_installer
+
+#endif  // COMMON_STEP_CONFIGURATION_STEP_BLOCK_CROSS_UPDATE_H_
--- src/common/step/configuration/step_configure.cc
+++ src/common/step/configuration/step_configure.cc
@@ -73,6 +73,22 @@ Step::Status StepConfigure::process() {
       context_->file_path.set(pkgmgr_->GetRequestInfo());
       context_->pkgid.set(kStrEmpty);
       break;
+    case RequestType::MountInstall:
+      context_->file_path.set(pkgmgr_->GetRequestInfo());
+      context_->pkgid.set(kStrEmpty);
+      if (!pkgmgr_->GetTepPath().empty()) {
+        context_->tep_path.set(pkgmgr_->GetTepPath());
+        context_->is_tep_move.set(pkgmgr_->GetIsTepMove());
+      }
+      break;
+    case RequestType::MountUpdate:
+      context_->file_path.set(pkgmgr_->GetRequestInfo());
+      context_->pkgid.set(kStrEmpty);
+      if (!pkgmgr_->GetTepPath().empty()) {
+        context_->tep_path.set(pkgmgr_->GetTepPath());
+        context_->is_tep_move.set(pkgmgr_->GetIsTepMove());
+      }
+      break;
     case RequestType::ManifestDirectInstall:
     case RequestType::ManifestDirectUpdate: {
       context_->pkgid.set(pkgmgr_->GetRequestInfo());
