5da1f393f56ef9758eed5daa0139825d9316dcd3
Tomasz Iwanek <t.iwanek@samsung.com>
2016-04-08 15:48:53 +0200

    Mount install steps
    Following changes should be submitted together:\
     - TODO: list app-installers, tpk-backend, wgt-backend changes
    
    Change-Id: I37974991f0d5fa2248e0a40aa1a999cc389f7485

---
+++ src/common/step/mount/step_umount_rollback.cc
@@ -0,0 +1,30 @@
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache-2.0 license that can be
+// found in the LICENSE file.
+
+#include "common/step/mount/step_umount_rollback.h"
+
+#include <boost/filesystem/path.hpp>
+
+#include <string>
+
+#include "common/backup_paths.h"
+#include "common/tzip_interface.h"
+
+namespace bf = boost::filesystem;
+
+namespace common_installer {
+namespace mount {
+
+Step::Status StepUmountRollback::undo() {
+  bf::path mount_point = GetMountLocation(context_->pkg_path.get());
+  TzipInterface tzip(mount_point.string());
+  if (!tzip.UnMountZip()) {
+    LOG(ERROR) << "Failed to umount: " << mount_point;
+    return Status::APP_DIR_ERROR;
+  }
+  return Status::OK;
+}
+
+}  // namespace mount
+}  // namespace common_installer
