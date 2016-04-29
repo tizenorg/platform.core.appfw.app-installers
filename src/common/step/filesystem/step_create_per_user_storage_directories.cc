fcfc624b31f3d6eeefbe9a7843400d3ec19e6b81
Sangyoon Jang <s89.jang@samsung.com>
2016-04-29 17:10:31 +0900

    Rework pkgdir-tool
    For several issue(performace, code consistency, ...), the pkgdir-tool is
    reworked.
    
    The app-installer calls security-manager api just once for labeling
    skel directories, not for each users.
    And the pkgdir-tool copies those labeled directories to each user's home
    directory.
    
    The pkgdir-tool doesn't have setuid bit no more(because of security issue),
    so it will be launched by dbus activation.
    (It still needs root privilege for writing permission to user directories)
    
    Change-Id: Ib96c41f08d681443c5993bedd3fcf4f1c91db8c3
    Signed-off-by: Sangyoon Jang <s89.jang@samsung.com>

--- src/common/step/filesystem/step_create_per_user_storage_directories.cc
+++ src/common/step/filesystem/step_create_per_user_storage_directories.cc
@@ -21,13 +21,12 @@ common_installer::Step::Status StepCreatePerUserStorageDirectories::process() {
   std::string package_id = context_->pkgid.get();
   LOG(INFO) << "Creating per-user directories for package: " << package_id;
 
-  Subprocess pkgdir_tool_process("/usr/bin/pkgdir-tool");
-  pkgdir_tool_process.RunWithArgs({"--create", "--pkgid", package_id});
-  int result = pkgdir_tool_process.Wait();
-  if (result) {
-    LOG(ERROR) << "Failed to create shared dirs for users";
+  if (!common_installer::CreateSkelDirectories(package_id))
     return Status::APP_DIR_ERROR;
-  }
+
+  if (!common_installer::RequestCopyUserDirectories(package_id))
+    return Status::APP_DIR_ERROR;
+
   return Status::OK;
 }
 
