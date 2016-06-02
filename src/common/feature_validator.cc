8e56108e63b0a0be5976e53a56aee199a920c242
Tomasz Iwanek <t.iwanek@samsung.com>
2016-06-02 11:54:12 +0200

    Validate features
    Validation is added in StepParseManifest instead of:
     - parser - parsing should not check platform details
     - separate step - there is no feature field in manifest_x
    
    Verification requires installing tpk package with some features and testing
    via manipulating /etc/config/model-config.xml
    
    Feature depending on type:
     - if it is string, string must match,
     - if it is integer, integer value on device must be at least as high,
     - if it is boolean, device must declare support if feature is set to true or empty.
    
    Requires:
     - https://review.tizen.org/gerrit/72780
    
    Change-Id: I521c0e18f76bf7eeecc56ee10cb31ce9f848378a

---
+++ src/common/feature_validator.cc
@@ -0,0 +1,120 @@
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache-2.0 license that can be
+// found in the LICENSE file.
+
+#include "common/feature_validator.h"
+
+#include <boost/scope_exit.hpp>
+
+#include <system_info.h>
+
+namespace {
+
+const char kTrueFeature[] = "true";
+
+}  // namespace
+
+namespace common_installer {
+
+FeatureValidator::FeatureValidator(
+    const std::map<std::string, std::string>& features)
+    : features_(features) {
+}
+
+bool FeatureValidator::Validate(std::string* error) {
+  for (auto& pair : features_) {
+    const auto& feature = pair.first;
+    const auto& value = pair.second;
+
+    system_info_type_e type;
+    int ret = system_info_get_platform_type(feature.c_str(), &type);
+    if (ret != SYSTEM_INFO_ERROR_NONE) {
+      *error = std::string("Unknown feature: ") + feature;
+      return false;
+    }
+    bool ok = false;
+    switch (type) {
+    case SYSTEM_INFO_BOOL:
+      ok = ValidateBoolean(feature, value, error);
+      break;
+    case SYSTEM_INFO_INT:
+      ok = ValidateInteger(feature, value, error);
+      break;
+    case SYSTEM_INFO_STRING:
+      ok = ValidateString(feature, value, error);
+      break;
+    case SYSTEM_INFO_DOUBLE:
+      // There is no double typed feature on platform, no way to interpret it
+      ok = true;
+      break;
+    default:
+      ok = false;
+      *error = "Unknown type of feature";
+    }
+    if (!ok)
+      return false;
+  }
+  return true;
+}
+
+bool FeatureValidator::ValidateBoolean(
+    const std::string& feature, const std::string& value, std::string* error) {
+  bool supported = false;
+  int ret = system_info_get_platform_bool(feature.c_str(), &supported);
+  if (ret != SYSTEM_INFO_ERROR_NONE) {
+    *error = std::string("Failed to call system_info_get_platform_bool()") +
+        " for " + feature + ", error code: " + std::to_string(ret);
+    return false;
+  }
+  if ((value.empty() || value == kTrueFeature) && !supported) {
+    *error = std::string("Feature '") + feature + " is not supported";
+    return false;
+  }
+  return true;
+}
+
+bool FeatureValidator::ValidateInteger(
+    const std::string& feature, const std::string& value, std::string* error) {
+  int platform_value = 0;
+  int ret = system_info_get_platform_int(feature.c_str(), &platform_value);
+  if (ret != SYSTEM_INFO_ERROR_NONE) {
+    *error = std::string("Failed to call system_info_get_platform_int()") +
+        " for " + feature + ", error code: " + std::to_string(ret);
+    return false;
+  }
+  int required_minimum = 0;
+  try {
+    required_minimum = std::stoi(value);
+  } catch (const std::logic_error&) {
+     return false;
+  }
+  if (required_minimum > platform_value) {
+    *error = std::string("Feature '") + feature + "' has value '" +
+        value + "' while platform declares only '" +
+        std::to_string(platform_value) + "'";
+    return false;
+  }
+  return true;
+}
+
+bool FeatureValidator::ValidateString(
+    const std::string& feature, const std::string& value, std::string* error) {
+  char* text = nullptr;
+  BOOST_SCOPE_EXIT_ALL(text) {
+    free(text);
+  };
+  int ret = system_info_get_platform_string(feature.c_str(), &text);
+  if (ret != SYSTEM_INFO_ERROR_NONE) {
+    *error = std::string("Failed to call system_info_get_platform_string()") +
+        " for " + feature + ", error code: " + std::to_string(ret);
+    return false;
+  }
+  if (value != text) {
+    *error = std::string("Feature '") + feature + "' has value '" + value +
+        "' while platform declares '" + std::string(text) + "'";
+    return false;
+  }
+  return true;
+}
+
+}  // namespace common_installer
