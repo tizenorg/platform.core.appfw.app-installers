32219b22c78997c929212affac82e4da2c99682b
Junghyun Yeon <jungh.yeon@samsung.com>
2016-06-02 09:35:40 +0900

    Implement pkg move
    Change-Id: I57bb2b5deffd5294010e8b0a2512f992cb3f466a
    Signed-off-by: Junghyun Yeon <jungh.yeon@samsung.com>

--- src/common/step/filesystem/step_move_installed_storage.cc
+++ src/common/step/filesystem/step_move_installed_storage.cc
@@ -1,36 +1,51 @@
-// Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
 // Use of this source code is governed by an apache-2.0 license that can be
 // found in the LICENSE file.
 
-#include "common/step/filesystem/step_acquire_external_storage.h"
+#include "common/step/filesystem/step_move_installed_storage.h"
 
 #include "common/external_storage.h"
 
 namespace {
 
-const char kInstalledExternally[] = "installed_external";
-const char kPreferExternal[] = "prefer-external";
-
-enum class Storage {
-  NONE,
-  INTERNAL,
-  EXTERNAL
-};
 
 }  // namespace
 
 namespace common_installer {
 namespace filesystem {
 
-Step::Status StepAcquireExternalStorage::precheck() {
+Step::Status StepMoveInstalledStorage::precheck() {
+#if 1
+  // if don't need to check anything, remove this
+  return Status::OK;
+#else
   if (!context_->manifest_data.get()) {
     LOG(ERROR) << "Manifest data is empty";
     return Status::ERROR;
   }
   return Status::OK;
+#endif
 }
 
-Step::Status StepAcquireExternalStorage::process() {
+Step::Status StepMoveInstalledStorage::process() {
+#if 1
+
+  //should we store returned object to context_->external_storage?
+  context_->external_storage =
+      ExternalStorage::MoveInstalledStorage(context_->request_type.get(),
+          context_->root_application_path.get(),
+          context_->pkgid.get(),
+          context_->pkg_type.get(),
+          context_->uid.get(),
+          context_->is_move_to_external.get());
+  if (!context_->external_storage) {
+    LOG(ERROR) << "Cannot initialize external storage for move";
+    return Status::APP_DIR_ERROR;
+  }
+
+  return Status::OK;
+
+#else
   manifest_x* manifest = context_->manifest_data.get();
   manifest_x* old_manifest = context_->old_manifest_data.get();
 
@@ -52,7 +67,8 @@ Step::Status StepAcquireExternalStorage::process() {
             context_->pkgid.get(),
             context_->pkg_type.get(),
             context_->unpacked_dir_path.get(),
-            context_->uid.get());
+            context_->uid.get(),
+            context_->is_move_to_external.get());
   }
 
   if (storage == Storage::EXTERNAL && !context_->external_storage) {
@@ -69,6 +85,7 @@ Step::Status StepAcquireExternalStorage::process() {
   } else {
     LOG(INFO) << "Package storage: internal";
   }
+#endif
   return Status::OK;
 }
 
