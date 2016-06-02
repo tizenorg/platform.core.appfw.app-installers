d2b441f230d5ea32747875b0ac3120e508f5a9bb
Junghyun Yeon <jungh.yeon@samsung.com>
2016-06-02 09:35:40 +0900

    Implement pkg move
    Releated change : tpk-backend
    https://review.tizen.org/gerrit/#/c/72618/
    
    Change-Id: I57bb2b5deffd5294010e8b0a2512f992cb3f466a
    Signed-off-by: Junghyun Yeon <jungh.yeon@samsung.com>
    Signed-off-by: jongmyeongko <jongmyeong.ko@samsung.com>

---
+++ src/common/step/filesystem/step_move_installed_storage.h
@@ -0,0 +1,30 @@
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by an apache-2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_STEP_FILESYSTEM_STEP_MOVE_INSTALLED_STORAGE_H_
+#define COMMON_STEP_FILESYSTEM_STEP_MOVE_INSTALLED_STORAGE_H_
+
+#include "common/installer_context.h"
+#include "common/step/step.h"
+
+namespace common_installer {
+namespace filesystem {
+
+class StepMoveInstalledStorage : public Step {
+ public:
+  using Step::Step;
+
+  Status process() override;
+
+  Status clean() override { return Status::OK; }
+  Status undo() override { return Status::OK; }
+  Status precheck() override { return Status::OK; }
+
+  STEP_NAME(MoveInstalledStorage)
+};
+
+}  // namespace filesystem
+}  // namespace common_installer
+
+#endif  // COMMON_STEP_FILESYSTEM_STEP_MOVE_INSTALLED_STORAGE_H_
