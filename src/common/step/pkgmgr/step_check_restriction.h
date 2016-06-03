f3c1440caa6264c5fb996910f57dd1e4f89d612a
Sangyoon Jang <s89.jang@samsung.com>
2016-06-03 17:30:00 +0900

    Add StepCheckRestriction
    Change-Id: If31097047c03df9a5d51ed6a4ced3a220e3781ad
    Signed-off-by: Sangyoon Jang <s89.jang@samsung.com>

---
+++ src/common/step/pkgmgr/step_check_restriction.h
@@ -0,0 +1,30 @@
+// Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+// Use of this source code is governed by a apache 2.0 license that can be
+// found in the LICENSE file.
+
+#ifndef COMMON_STEP_PKGMGR_STEP_CHECK_RESTRICTION_H_
+#define COMMON_STEP_PKGMGR_STEP_CHECK_RESTRICTION_H_
+
+#include <manifest_parser/utils/logging.h>
+
+#include "common/step/step.h"
+
+namespace common_installer {
+namespace pkgmgr {
+
+class StepCheckRestriction : public Step {
+ public:
+  using Step::Step;
+
+  Status process() override;
+  Status clean() override { return Status::OK; }
+  Status undo() override { return Status::OK; }
+  Status precheck() override { return Status::OK; }
+
+  STEP_NAME(CheckRestriction)
+};
+
+}  // namespace pkgmgr
+}  // namespace common_installer
+
+#endif  // COMMON_STEP_PKGMGR_STEP_CHECK_RESTRICTION_H_
