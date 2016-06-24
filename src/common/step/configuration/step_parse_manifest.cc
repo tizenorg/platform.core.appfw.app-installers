0d053578d285f246eaaa1e8b81e7a70f73172c67
jongmyeongko <jongmyeong.ko@samsung.com>
2016-06-24 11:12:14 +0900

    block CheckFeature
    Change-Id: I39d0e2c37a498a691b8c9590d97926cde8d2aa20
    Signed-off-by: jongmyeongko <jongmyeong.ko@samsung.com>

--- src/common/step/configuration/step_parse_manifest.cc
+++ src/common/step/configuration/step_parse_manifest.cc
@@ -806,8 +806,9 @@ Step::Status StepParseManifest::process() {
     return Step::Status::PARSE_ERROR;
   }
 
-  if (!CheckFeatures())
-    return Status::PARSE_ERROR;
+  // TODO: need to be checked more (offline mode and store app)
+  //if (!CheckFeatures())
+  //  return Status::PARSE_ERROR;
 
   if (manifest_location_ == ManifestLocation::INSTALLED) {
     // recovery of tep value for installed package
